46f25b6d0209bde39e13505a29e23db41dba18df
Nitsan Avni <nitsanav@gmail.com>
2022-02-04 15:31:01 +0000

    add a tie breaker

--- closest-to-zero/test.ts
+++ closest-to-zero/test.ts
@@ -2,26 +2,44 @@ import test from "ava";
 import fc from "fast-check";
 import { inspect } from "util";
 
-const minBy = <T>(arr: T[], mapper: (t: T) => number): T | undefined => {
+const minBy = <T>(
+  arr: T[],
+  mapper: (t: T) => number,
+  tieBreaker: (a: T, b: T) => T
+): T | undefined => {
   let ret = arr[0];
 
   for (const t of arr) {
+    // we could avoid running the mapper four times by caching results
     if (mapper(t) < mapper(ret)) {
       ret = t;
+    } else if (mapper(t) === mapper(ret)) {
+      ret = tieBreaker(t, ret);
     }
   }
 
   return ret;
 };
 
-const closestToZero = (arr: number[]) => minBy(arr, Math.abs);
+const closestToZero = (arr: number[]) => minBy(arr, Math.abs, Math.max);
 
-test.todo("tie break - take the positive one");
+test("tie break - take the positive one", (t) => {
+  const { failed, counterexample } = fc.check(
+    fc.property(
+      fc
+        .array(fc.integer(), { minLength: 1 })
+        .map((arr) => [...arr, ...arr.map((x) => -x)]),
+      (arr) => Math.min(...arr.map(Math.abs)) === closestToZero(arr)!
+    )
+  );
+
+  t.false(failed, `counter example: ${inspect(counterexample)}`);
+});
 
 test("absolute value of result equals min of absolute values", (t) => {
   const { failed, counterexample } = fc.check(
     fc.property(
-      fc.array(fc.integer(), { minLength: 0 }),
+      fc.array(fc.integer(), { minLength: 1 }),
       (arr) => Math.min(...arr.map(Math.abs)) === Math.abs(closestToZero(arr)!)
     )
   );
