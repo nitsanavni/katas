6efe560a6c7042ebed06ae70544c7011c96c5d07
Nitsan Avni <nitsanav@gmail.com>
2022-04-11 15:26:27 +0000

    wip traefik getting started

--- traefik-exploration/traefik.test.ts
+++ traefik-exploration/traefik.test.ts
@@ -1,10 +1,46 @@
 import { promisify } from "util";
 import test from "ava";
 import { GenericContainer, Network, Wait } from "testcontainers";
-import { Mountebank } from "@anev/ts-mountebank";
-import { $ } from "zx";
+import {
+  EqualPredicate,
+  HttpMethod,
+  Imposter,
+  Mountebank,
+  Response,
+  Stub,
+} from "@anev/ts-mountebank";
+import { $, sleep } from "zx";
+$.verbose = false;
 
 const tick = promisify(setTimeout);
+
+test.todo("traefik proxies calls to mountebank imposter");
+
+test("traefik getting started, only instead of docker-compose we use testcontainers", async (t) => {
+  // https://doc.traefik.io/traefik/getting-started/quick-start/#launch-traefik-with-the-docker-provider
+
+  const network = (await new Network().start()).getName();
+
+  const traefik = await new GenericContainer("traefik:v2.6")
+    .withNetworkMode(network)
+    .withCmd(["--api.insecure=true", "--providers.docker"])
+    .withExposedPorts(80, 8080)
+    .withBindMount("/var/run/docker.sock", "/var/run/docker.sock")
+    .start();
+
+  (await traefik.logs()).pipe(process.stderr);
+
+  const whoami = await new GenericContainer("traefik/whoami")
+    .withNetworkMode(network)
+    .start();
+
+  // need to add a label, but testcontainers api doesn't have it (yet)
+  // maybe use the docker cli
+
+  await whoami.getId();
+
+  await sleep(10000);
+});
 
 test("mountebank imposters in a docker network", async (t) => {
   const network = (
@@ -12,6 +48,7 @@ test("mountebank imposters in a docker network", async (t) => {
   ).getName();
 
   const mountebank = await new GenericContainer("bbyars/mountebank:2.6.0")
+    .withName("mb")
     .withExposedPorts(2525)
     .withNetworkMode(network)
     .withNetworkAliases("mb", "imposter")
@@ -21,9 +58,10 @@ test("mountebank imposters in a docker network", async (t) => {
     )
     .start();
 
-  $`docker ps`.stdout.pipe(process.stdout);
-
-  console.log(`mapped port: ${mountebank.getMappedPort(2525)}`);
+  t.regex(
+    (await $`docker ps`).stdout,
+    new RegExp(`${mountebank.getMappedPort(2525)}->2525.*mb`, "s")
+  );
 
   const logsStream = await mountebank.logs();
 
@@ -39,57 +77,35 @@ test("mountebank imposters in a docker network", async (t) => {
     /installing curl.*ok/is
   );
 
-  // create the imposter
-  for (let i = 0; i < 10; i++) {
-    t.regex(
-      (
-        await caller.exec([
-          "curl",
-          "-i",
-          "-X",
-          "POST",
-          "-H",
-          "Content-Type: application/json",
-          "http://mb:2525/imposters",
-          "--data",
-          JSON.stringify({
-            port: 4545 + i,
-            protocol: "http",
-            stubs: [
-              {
-                responses: [
-                  {
-                    is: {
-                      statusCode: 200,
-                      body: `hello from imposter #${i}`,
-                      _mode: "text",
-                    },
-                  },
-                ],
-                predicates: [{ equals: { path: "/test", method: "GET" } }],
-              },
-            ],
-          }),
-        ])
-      ).output,
-      /created/i
-    );
-  }
-
-  for (let i = 0; i < 10; i++) {
-    t.deepEqual(
-      (await caller.exec(["curl", `http://imposter:${4545 + i}/test`])).output,
-      `hello from imposter #${i}`
-    );
-  }
-
   const mb = new Mountebank().withURL(
     `http://localhost:${mountebank.getMappedPort(2525)}`
   );
 
-  const imposter = await mb.getImposter(4545);
+  const stubBody = "hello from imposter";
 
-  t.deepEqual(JSON.stringify(imposter), "a");
+  await mb.createImposter(
+    new Imposter()
+      .withName("imposter a")
+      .withRecordRequests(true)
+      .withPort(4600)
+      .withStub(
+        new Stub()
+          .withPredicate(
+            new EqualPredicate().withPath("/a").withMethod(HttpMethod.GET)
+          )
+          .withResponse(new Response().withStatusCode(200).withBody(stubBody))
+      )
+  );
+
+  t.deepEqual(
+    (await caller.exec(["curl", `http://imposter:4600/a`])).output,
+    stubBody
+  );
+
+  const imposter = await mb.getImposter(4600);
+
+  t.like(imposter, { numberOfRequests: 1 });
+  t.like(imposter.requests[0], { method: "GET", path: "/a" });
 });
 
 test("up a docker container", async (t) => {
