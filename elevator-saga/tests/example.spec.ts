557a2ae36f21cf1793b02cc8295a2b782b4fd227
Nitsan Avni <nitsanav@gmail.com>
2022-04-30 17:01:00 +0000

    clean up

--- elevator-saga/tests/example.spec.ts
+++ elevator-saga/tests/example.spec.ts
@@ -1,14 +1,12 @@
-import { test, expect, Page } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://play.elevatorsaga.com/");
-  await page.waitForLoadState("domcontentloaded");
 });
 
 test("sanity - can reach site, sees first elevator challenge", async ({
   page,
 }) => {
-  await page.screenshot({ path: "./1.png" });
   expect((await page.locator("h3").allTextContents())[0]).toMatch(
     "Challenge #1"
   );
