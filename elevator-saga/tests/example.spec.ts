055f11b2be16e1791616c683d996ec0b5cd3c0e2
Nitsan Avni <nitsanav@gmail.com>
2022-04-30 17:09:21 +0000

    wip start the emulator

--- elevator-saga/tests/example.spec.ts
+++ elevator-saga/tests/example.spec.ts
@@ -4,9 +4,17 @@ test.beforeEach(async ({ page }) => {
   await page.goto("http://play.elevatorsaga.com/");
 });
 
+test("speed up, start, and fail", async ({ page }) => {
+  expect(await page.locator(".challenge > h3 > span")).toHaveText("2x");
+  await page.click(".timescale_increase");
+});
+
 test("sanity - can reach site, sees first elevator challenge", async ({
   page,
 }) => {
+  expect(await page.title()).toBe(
+    "Elevator Saga - the elevator programming game"
+  );
   expect((await page.locator("h3").allTextContents())[0]).toMatch(
     "Challenge #1"
   );
