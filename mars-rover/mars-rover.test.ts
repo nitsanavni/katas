f8a5452522f7badd2b673e000c647f530391c31d
Nitsan Avni <nitsanav@gmail.com>
2022-06-06 09:03:38 +0000

    first outer loop TDD test passes

--- mars-rover/mars-rover.test.ts
+++ mars-rover/mars-rover.test.ts
@@ -1,18 +1,84 @@
 import test from "ava";
+import os from "os";
+
+type Orientation = "N" | "E" | "S" | "W";
 
 type RoverState = {
   x: number;
   y: number;
-  orientation: "N" | "E" | "S" | "W";
+  orientation: Orientation;
 };
 
 const parseState = (stateString: string): RoverState => {
-  return { x: 0, y: 0, orientation: "N" };
+  const [, x, y, orientation] = /^(\d+) (\d+) ([NESW])$/.exec(stateString)!;
+
+  return { x: +x, y: +y, orientation } as RoverState;
+};
+
+type Instruction = "M" | "L" | "R";
+
+const parseInstructions = (instructionsString: string): Instruction[] =>
+  instructionsString.split("") as Instruction[];
+
+const format = ({ x, y, orientation }: RoverState): string =>
+  `${x} ${y} ${orientation}`;
+
+const moveRover = ({
+  from,
+  instruction,
+}: {
+  from: RoverState;
+  instruction: Instruction;
+}): RoverState => {
+  return { ...from, y: from.y + 1 };
 };
 
 const play = (input: string) => {
-  return "0 0 N";
+  const [, initStateString, instructionsString] = input.split(os.EOL);
+
+  let state = parseState(initStateString);
+
+  const instructions = parseInstructions(instructionsString);
+
+  for (const instruction of instructions) {
+    state = moveRover({ from: state, instruction });
+  }
+
+  return format(state);
 };
+
+test("format state - 1 2 W", (t) => {
+  t.deepEqual(format({ x: 1, y: 2, orientation: "W" }), "1 2 W");
+});
+
+test("format state - 0 0 N", (t) => {
+  t.deepEqual(format({ x: 0, y: 0, orientation: "N" }), "0 0 N");
+});
+
+test("move rover - 0 0 N -> M -> 0 1 N", (t) => {
+  t.deepEqual(
+    format(moveRover({ from: parseState("0 0 N"), instruction: "M" })),
+    "0 1 N"
+  );
+});
+
+test("parse instructions - MRL", (t) => {
+  const instructions = parseInstructions("MRL");
+
+  t.deepEqual(instructions, ["M", "R", "L"]);
+});
+
+test("parse instructions - M", (t) => {
+  const instructions = parseInstructions("M");
+
+  t.deepEqual(instructions, ["M"]);
+});
+
+test("parse state - 1 2 E", (t) => {
+  const state = parseState("1 2 E");
+
+  t.deepEqual(state, { x: 1, y: 2, orientation: "E" });
+});
 
 test("parse state - 0 0 N", (t) => {
   const state = parseState("0 0 N");
@@ -20,7 +86,7 @@ test("parse state - 0 0 N", (t) => {
   t.deepEqual(state, { x: 0, y: 0, orientation: "N" });
 });
 
-test.failing("single rover - should move north by 1", (t) => {
+test("single rover - should move north by 1", (t) => {
   const upperRightCoordinates = "0 1";
   const roverInitialState = "0 0 N";
   // no instructions at all
