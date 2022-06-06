293a85ebce735560d2a6cfc8c600c8dddb42ee0e
Nitsan Avni <nitsanav@gmail.com>
2022-06-06 09:28:04 +0000

    start moving about

--- mars-rover/mars-rover.test.ts
+++ mars-rover/mars-rover.test.ts
@@ -23,15 +23,34 @@ const parseInstructions = (instructionsString: string): Instruction[] =>
 const format = ({ x, y, orientation }: RoverState): string =>
   `${x} ${y} ${orientation}`;
 
+const moveNorth = (from: RoverState): RoverState => ({
+  ...from,
+  y: from.y + 1,
+});
+
+const moveEast = (from: RoverState): RoverState => ({
+  ...from,
+  x: from.x + 1,
+});
+
+const spinRight = (from: RoverState): RoverState => ({
+  ...from,
+  orientation: "E",
+});
+
+const moveForward = (from: RoverState): RoverState =>
+  ({ N: moveNorth, E: moveEast, S: moveNorth, W: moveNorth }[from.orientation](
+    from
+  ));
+
 const moveRover = ({
   from,
   instruction,
 }: {
   from: RoverState;
   instruction: Instruction;
-}): RoverState => {
-  return { ...from, y: from.y + 1 };
-};
+}): RoverState =>
+  ({ M: moveForward, R: spinRight, L: spinRight }[instruction](from));
 
 const play = (input: string) => {
   const [, initStateString, instructionsString] = input.split(os.EOL);
@@ -53,6 +72,20 @@ test("format state - 1 2 W", (t) => {
 
 test("format state - 0 0 N", (t) => {
   t.deepEqual(format({ x: 0, y: 0, orientation: "N" }), "0 0 N");
+});
+
+test("move rover east", (t) => {
+  t.deepEqual(
+    format(moveRover({ from: parseState("0 0 E"), instruction: "M" })),
+    "1 0 E"
+  );
+});
+
+test("move rover - 0 0 N -> R -> 0 0 E", (t) => {
+  t.deepEqual(
+    format(moveRover({ from: parseState("0 0 N"), instruction: "R" })),
+    "0 0 E"
+  );
 });
 
 test("move rover - 0 0 N -> M -> 0 1 N", (t) => {
@@ -86,10 +119,61 @@ test("parse state - 0 0 N", (t) => {
   t.deepEqual(state, { x: 0, y: 0, orientation: "N" });
 });
 
+test("single rover - 0 0 N -> RM -> 1 0 E", (t) => {
+  const upperRightCoordinates = "0 0";
+  const roverInitialState = "0 0 N";
+  const roverInstructions = "RM";
+
+  const input = `${upperRightCoordinates}
+${roverInitialState}
+${roverInstructions}`;
+
+  const output = play(input);
+
+  // expect no change in state, as no instructions where given
+  const expectedRoverFinalState = "1 0 E";
+
+  t.deepEqual(output, expectedRoverFinalState);
+});
+
+test("single rover - should spin left", (t) => {
+  const upperRightCoordinates = "0 0";
+  const roverInitialState = "0 0 N";
+  const roverInstructions = "R";
+
+  const input = `${upperRightCoordinates}
+${roverInitialState}
+${roverInstructions}`;
+
+  const output = play(input);
+
+  // expect no change in state, as no instructions where given
+  const expectedRoverFinalState = "0 0 E";
+
+  t.deepEqual(output, expectedRoverFinalState);
+});
+
+test("single rover - should move north by 2", (t) => {
+  const upperRightCoordinates = "0 2";
+  const roverInitialState = "0 0 N";
+  const roverInstructions = "MM";
+
+  const input = `${upperRightCoordinates}
+${roverInitialState}
+${roverInstructions}`;
+
+  const output = play(input);
+
+  // expect no change in state, as no instructions where given
+  const expectedRoverFinalState = "0 2 N";
+
+  t.deepEqual(output, expectedRoverFinalState);
+});
+
 test("single rover - should move north by 1", (t) => {
   const upperRightCoordinates = "0 1";
   const roverInitialState = "0 0 N";
-  // no instructions at all
+  // just move one step forward
   const roverInstructions = "M";
 
   const input = `${upperRightCoordinates}
