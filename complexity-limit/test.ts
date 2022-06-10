5f2c69dea99a01541d00af69ea8a9b7089fb879c
Nitsan Avni <nitsanav@gmail.com>
2022-06-10 14:12:42 +0000

    2nd attempt - using a more declarative style

--- complexity-limit/test.ts
+++ complexity-limit/test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { constant, each, identity, noop } from "lodash";
+import { constant, each, find, identity, noop, reduce } from "lodash";
 
 // fizzbuzz(n) = n
 // divisable by 3 - fizz
@@ -29,22 +29,41 @@ const checkThree = checkFor(divisable(3))(constant("fizz"));
 const checkFive = checkFor(divisable(5))(constant("buzz"));
 const checkBoth = checkFor(divisable(15))(constant("fizzbuzz"));
 
-const fizzbuzz: FizzBuzzer = checkBoth(checkThree(checkFive(standard(stop))));
+const fizzbuzzComposedFunctions: FizzBuzzer = checkBoth(
+  checkThree(checkFive(standard(stop)))
+);
 
-each(
-  [
-    [1, 1],
-    [2, 2],
-    [3, "fizz"],
-    [4, 4],
-    [5, "buzz"],
-    [6, "fizz"],
-    [7, 7],
-    [8, 8],
-    [9, "fizz"],
-    [10, "buzz"],
-    [15, "fizzbuzz"],
-  ] as const,
-  ([n, expected]) =>
-    test(`fizzbuzz(${n}): ${expected}`, (t) => t.is(fizzbuzz(n), expected))
+const fizzbuzzFind: FizzBuzzer = (n: number) => {
+  const [, fizzbuzzer] = find(
+    [
+      [divisable(15), constant("fizzbuzz")],
+      [divisable(3), constant("fizz")],
+      [divisable(5), constant("buzz")],
+      [constant(true), identity],
+    ] as [Predicate, FizzBuzzer][],
+    ([predicate]) => predicate(n)
+  )!;
+
+  return fizzbuzzer(n);
+};
+
+each([fizzbuzzFind, fizzbuzzComposedFunctions], (fizzbuzz) =>
+  each(
+    [
+      [1, 1],
+      [2, 2],
+      [3, "fizz"],
+      [4, 4],
+      [5, "buzz"],
+      [6, "fizz"],
+      [7, 7],
+      [8, 8],
+      [9, "fizz"],
+      [10, "buzz"],
+      [15, "fizzbuzz"],
+    ] as const,
+    ([n, expected]) =>
+      test(`${fizzbuzz.name}(${n}): ${expected}`, (t) =>
+        t.is(fizzbuzz(n), expected))
+  )
 );
--- complexity-limit/thoughts.md
+++ complexity-limit/thoughts.md
@@ -1,6 +1,8 @@
 - I still don't fully grasp why `flow` doesn't work, it's something to do with the functions being too high order I think
-- my current conclusion is that you can't go below a max complexity of 2
+- ~~my current conclusion is that you can't go below a max complexity of 2~~
+- updated conclusion - you can push complexity to lodash, and have own code be with complexity 1
 - higher order functions are not easy to read
 - I think I'm missing some simplifying rules for fp
 - did not commit often (almost not at all)
 - should probably prune the tests - according to https://jay.bazuzi.com/Characteristics-of-Ideal-Tests/ - "1 defect == 1 test failure"
+- can be done using ~~`reduce`~~ `find` - done - reached complexity of 1
