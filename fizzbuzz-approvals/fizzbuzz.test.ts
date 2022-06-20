54311b527f33192801c74a39e2751fcbb2846a9d
Nitsan Avni <nitsanav@gmail.com>
2022-06-20 18:52:42 +0000

    fizzbuzz

--- fizzbuzz-approvals/fizzbuzz.test.ts
+++ fizzbuzz-approvals/fizzbuzz.test.ts
@@ -1,17 +1,15 @@
+import { EOL } from "os";
 import test from "ava";
 import { range } from "lodash";
 
 type FizzBuzz = number | "Fizz" | "Buzz" | "FizzBuzz";
 
-const fizzbuzz = (n: number): FizzBuzz => {
-  return n % 3 == 0 ? "Fizz" : n % 5 == 0 ? "Buzz" : n;
-};
+const fizzbuzz = (n: number): FizzBuzz =>
+  n % 15 == 0 ? "FizzBuzz" : n % 3 == 0 ? "Fizz" : n % 5 == 0 ? "Buzz" : n;
 
 const printer = (fb: typeof fizzbuzz): string =>
-  `|n|fizzbuzz(n)|
-    |---|---|
-    ${range(1, 101)
-      .map((n) => `|${n}|${fb(n)}|`)
-      .join("\n")}}`;
+  `|n|fizzbuzz(n)|${EOL}|---|---|${EOL}${range(1, 101)
+    .map((n) => `|${n}|${fb(n)}|`)
+    .join(EOL)}`;
 
 test("fizzbuzz", ({ snapshot }) => snapshot(printer(fizzbuzz)));
