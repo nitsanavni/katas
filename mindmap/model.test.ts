5306839d1280fef314e00f442efb6406bd396940
Nitsan Avni <nitsanav@gmail.com>
2022-07-03 07:33:50 +0000

    BehaviorSubject -> Subject

--- mindmap/model.test.ts
+++ mindmap/model.test.ts
@@ -1,6 +1,6 @@
 import test from "ava";
 import dedent from "dedent";
-import { BehaviorSubject } from "rxjs";
+import { Subject } from "rxjs";
 
 import formatMindmap from "./format-mindmap";
 import { makeModel } from "./model";
@@ -8,12 +8,12 @@ import { OutlineNode } from "./outline-node";
 import parse from "./parse";
 
 test("init", (t) => {
-  const model = makeModel({
-    initWith: new BehaviorSubject<OutlineNode[]>(
-      parse(dedent`0|root
-                   1|s|hello model!`)
-    ),
-  });
+  const initWith = new Subject<OutlineNode[]>();
+  const model = makeModel({ initWith });
+  const outline = dedent`0|root
+                         1|s|hello model!`;
+  const nodes = parse(outline);
+  initWith.next(nodes);
 
   t.snapshot(formatMindmap(model.get()));
 });
