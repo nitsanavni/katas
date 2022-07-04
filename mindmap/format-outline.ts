ec4120fe98b4dd9c68fe43fc01ed14b5013e2250
Nitsan Avni <nitsanav@gmail.com>
2022-07-04 12:40:26 +0000

    outline format supports collapsed

--- mindmap/format-outline.ts
+++ mindmap/format-outline.ts
@@ -3,8 +3,12 @@ import { OutlineNode } from "./outline-node";
 const formatOutlineLine = ({
   indentation,
   selected,
+  collapsed,
   body,
-}: OutlineNode): string => `${indentation}|${selected ? "s|" : ""}${body}`;
+}: OutlineNode): string =>
+  `${indentation}|${selected ? "s" : ""}${collapsed ? "c" : ""}${
+    selected || collapsed ? "|" : ""
+  }${body}`;
 
 export const formatOutline = (nodes: OutlineNode[]): string[] =>
   nodes.map(formatOutlineLine);
--- mindmap/parse.ts
+++ mindmap/parse.ts
@@ -6,15 +6,16 @@ import { OutlineNode } from "./outline-node";
 // exported for testing only
 export const parseLine = (line: string): OutlineNode | undefined => {
   // a line looks like this: `${indentation}|[${flags}|]${body}`
-  const m = /(\d+)\|((s)?\|)?(.*)/.exec(line)!;
+  const m = /(\d+)\|((s)?(c)?\|)?(.*)/.exec(line)!;
 
   if (!m) {
     return undefined;
   }
 
   return {
-    body: m[4],
+    body: m[5],
     ...(m[3] == "s" ? { selected: true } : {}),
+    ...(m[4] == "c" ? { collapsed: true } : {}),
     indentation: +m[1],
   };
 };
