8519e52469b4c1e5c847cde64015ae6be5830104
Nitsan Avni <nitsanav@gmail.com>
2022-09-11 11:12:34 +0000

    black .

--- approvals-in-gitpod/test_approvals_in_gitpod.py
+++ approvals-in-gitpod/test_approvals_in_gitpod.py
@@ -1,5 +1,10 @@
 from approvaltests import verify
 from approvaltests.reporters import GenericDiffReporter, create_config
 
+
 def test_approvals_in_gitpod():
-    verify("Hello world!", reporter=GenericDiffReporter(create_config(["gp", "code", ["-d"]])))
+    gp_reporter = GenericDiffReporter(create_config(["gp", "code", ["-d"]]))
+    verify(
+        "Hello world!",
+        reporter=gp_reporter,
+    )
