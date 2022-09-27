dc43087dc556f30aab5e8fc274b46fbd57e8de8f
Nitsan Avni <nitsanav@gmail.com>
2022-09-27 19:53:22 +0000

    get-catch2.sh

---
+++ c-mars-rover/get-catch2.sh
@@ -0,0 +1,2 @@
+curl -L -o catch_amalgamated.hpp https://github.com/catchorg/Catch2/releases/download/v3.1.0/catch_amalgamated.hpp
+curl -L -o catch_amalgamated.cpp https://github.com/catchorg/Catch2/releases/download/v3.1.0/catch_amalgamated.cpp
