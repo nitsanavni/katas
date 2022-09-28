77e651208b8b56850db6cc86fd48feac1431ec14
Nitsan Avni <nitsanav@gmail.com>
2022-09-28 09:22:02 +0000

    single rover no instructions

--- c-mars-rover/mars_rover.c
+++ c-mars-rover/mars_rover.c
@@ -1,3 +1,34 @@
 #include "mars_rover.h"
+#include <string.h>
 
-const char *mars_rover(const char *input) { return "1 3 N"; }
+const char *parse_initial_rover_position(const char *input);
+
+const char *next_newline_or_eos(const char *str);
+
+const char *mars_rover(const char *input) {
+  const char *position_string = parse_initial_rover_position(input);
+
+  return position_string;
+}
+
+const char *parse_initial_rover_position(const char *input) {
+  const char *newline = next_newline_or_eos(input);
+
+  if (*newline == '\0') {
+    return "";
+  }
+
+  const char *start = newline + 1;
+  const char *end = next_newline_or_eos(start);
+
+  return strndup(start, end - start);
+}
+
+const char *next_newline_or_eos(const char *str) {
+  char *position_start = (char *)str;
+
+  for (; *position_start != '\n' && *position_start != '\0'; position_start++) {
+  }
+
+  return position_start;
+}
--- c-mars-rover/test.cpp
+++ c-mars-rover/test.cpp
@@ -6,9 +6,27 @@ extern "C" {
 #include "mars_rover.h"
 }
 
-TEST_CASE("Mars Rover - example from web") {
+TEST_CASE("mars_rover: single rover, single instruction") {
   CHECK_THAT(mars_rover(R"(5 5
 1 2 N
-LMLMLMLMM)"),
+M)"),
              Equals("1 3 N"));
 }
+
+TEST_CASE("mars_rover: single rover, no instructions") {
+  CHECK_THAT(mars_rover(R"(5 5
+1 2 N
+)"),
+             Equals("1 2 N"));
+  CHECK_THAT(mars_rover(R"(5 5
+2 2 E
+)"),
+             Equals("2 2 E"));
+}
+
+// TEST_CASE("Mars Rover - example from web") {
+//   CHECK_THAT(mars_rover(R"(5 5
+// 1 2 N
+// LMLMLMLMM)"),
+//              Equals("1 3 N"));
+// }
