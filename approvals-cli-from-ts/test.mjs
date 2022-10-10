fb7094d417733290fec0e96363f801fb5d9f8317
Nitsan Avni <nitsanav@gmail.com>
2022-10-10 21:02:45 +0000

    fizzbuzz starting point

--- approvals-cli-from-ts/test.mjs
+++ approvals-cli-from-ts/test.mjs
@@ -1,5 +1,6 @@
 import test from "ava";
 import { $ } from "zx";
+import _ from "lodash";
 
 const verify = async ({ t, testId, received }) =>
   t.is(
@@ -10,4 +11,14 @@ const verify = async ({ t, testId, received }) =>
 
 test("hello world!", async (t) => {
   await verify({ t, testId: "hello", received: "hello world!" });
+});
+
+const fizzbuzz = (n) => n;
+
+test("fizzbuzz 1-100", async (t) => {
+  await verify({
+    t,
+    testId: "fizzbuzz-1-100",
+    received: _.range(1, 101).map(fizzbuzz).join("\n"),
+  });
 });
