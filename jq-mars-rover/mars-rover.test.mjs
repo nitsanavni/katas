4e4869f58ea3c70b33bb5159435f86d096635dc4
Nitsan Avni <nitsanav@gmail.com>
2022-10-12 13:18:16 +0000

    scaffold - run jaq

--- jq-mars-rover/mars-rover.test.mjs
+++ jq-mars-rover/mars-rover.test.mjs
@@ -1,6 +1,18 @@
 import test from "ava";
+import { $ } from "zx";
+
 import { verify } from "./verify.mjs";
 
-test("scaffold", async (t) => {
-  await verify(t, "hello scaffold");
+// Test Input:
+// 5 5
+// 1 2 N
+// LMLMLMLMM
+// 3 3 E
+// MMRMMRMRRM
+// Expected Output:
+// 1 3 N
+// 5 1 E
+
+test("mars rover - example from kata description", async (t) => {
+  await verify(t, (await $`jaq -n '3'`).stdout);
 });
