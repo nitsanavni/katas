908ea8aca973858e4f2f6f65c9c1d36b52870f3e
Nitsan Avni <nitsanav@gmail.com>
2022-10-12 13:34:12 +0000

    jaq sees all the input

--- jq-mars-rover/mars-rover.test.mjs
+++ jq-mars-rover/mars-rover.test.mjs
@@ -3,9 +3,7 @@ import { $ } from "zx";
 
 import { verify } from "./verify.mjs";
 
-
 async function streamToString(stream) {
-  // lets have a ReadableStream as a stream variable
   const chunks = [];
 
   for await (const chunk of stream) {
@@ -16,26 +14,24 @@ async function streamToString(stream) {
 }
 
 const marsRover = async (input) => {
-  const process = $`jaq -f mars-rover.jq`;
-  process.stdin.write(input);
+  const process = $`jaq -Rf mars-rover.jq`;
+
+  process.stdin.write(Buffer(input));
   process.stdin.end();
 
-  const result = await streamToString(process.stdout);
-  return result;
+  return await streamToString(process.stdout);
 };
 
-// Test Input:
-// 5 5
-// 1 2 N
-// LMLMLMLMM
-// 3 3 E
-// MMRMMRMRRM
-// Expected Output:
-// 1 3 N
-// 5 1 E
-
 test("mars rover - example from kata description", async (t) => {
-  const input = "3";
+  const input = `5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM`;
+
+  // Expected Output:
+  // 1 3 N
+  // 5 1 E
 
   const result = await marsRover(input);
 
