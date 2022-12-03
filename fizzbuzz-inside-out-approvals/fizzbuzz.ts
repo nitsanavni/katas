9a5da96c9c00a2e36e3e4917b085286c43e2543b
Nitsan Avni <nitsanav@gmail.com>
2022-12-03 09:29:39 +0000

    game, fizzbuzz

--- fizzbuzz-inside-out-approvals/fizzbuzz.ts
+++ fizzbuzz-inside-out-approvals/fizzbuzz.ts
@@ -6,6 +6,8 @@ import test from "./test";
 // "" | "Fizz" | "Buzz" | "FizzBuzz"
 // "" | "Fizz" | "Buzz" | "FizzBuzz" | number
 
+const s = JSON.stringify;
+
 const multipleOf: (d: number) => (n: number) => boolean = (d) => (n) =>
     n % d == 0;
 
@@ -58,51 +60,67 @@ const arr: <C>(s: Spec<C>[]) => (n: number) => string[] = (s) => {
     return (n: number) => codes.map((c) => c(n));
 };
 
+const ns = [2, 3, 4, 12];
+const specss = [
+    [{ d: 3, code: "fizz" }],
+    [{ d: 4, code: "foo" }],
+    [
+        { d: 3, code: "fizz" },
+        { d: 4, code: "foo" },
+    ],
+    [
+        { d: 4, code: "foo" },
+        { d: 3, code: "fizz" },
+    ],
+];
+
 test('["Fizz" | "", "Buzz" | ""]', () => {
-    const ns = [3, 4, 12];
-    const specss = [
-        [{ d: 3, code: "fizz" }],
-        [{ d: 4, code: "foo" }],
-        [
-            { d: 3, code: "fizz" },
-            { d: 4, code: "foo" },
-        ],
-        [
-            { d: 4, code: "foo" },
-            { d: 3, code: "fizz" },
-        ],
-    ];
-
-    const s = JSON.stringify;
-
     return product(specss, ns)
         .map(([specs, n]) => `arr(${s(specs)})(${n}):\n${s(arr(specs)(n))}`)
         .join("\n");
 });
 
-type F<A, R> = (a: A) => R;
-
 const codes: <C>(s: Spec<C>[]) => (n: number) => string = (s) => (n) =>
     arr(s)(n).join("");
 
 test('"" | "Fizz" | "Buzz" | "FizzBuzz"', () => {
-    const ns = [3, 4, 12];
-    const specss = [
-        [{ d: 3, code: "fizz" }],
-        [{ d: 4, code: "foo" }],
-        [
-            { d: 3, code: "fizz" },
-            { d: 4, code: "foo" },
-        ],
-        [
-            { d: 4, code: "foo" },
-            { d: 3, code: "fizz" },
-        ],
-    ];
-
-    const s = JSON.stringify;
-
     return product(specss, ns)
         .map(([specs, n]) => `codes(${s(specs)})(${n}):\n${s(codes(specs)(n))}`)
         .join("\n");
+});
+
+const game: <C>(s: Spec<C>[]) => (n: number) => string | number =
+    (s) => (n) => {
+        const c = codes(s)(n);
+
+        return c.length == 0 ? n : c;
+    };
+
+test('"" | "Fizz" | "Buzz" | "FizzBuzz" | number', () => {
+    return product(specss, ns)
+        .map(([specs, n]) => `game(${s(specs)})(${n}):\n${s(game(specs)(n))}`)
+        .join("\n");
+});
+
+const fizzbuzz = game([
+    { d: 3, code: "Fizz" },
+    { d: 5, code: "Buzz" },
+    { d: 7, code: "Whizz" },
+    { d: 11, code: "Bang" },
+]);
+
+const range = (n: number): number[] => {
+    const ret: number[] = [];
+
+    for (let i = 0; i < n; i++) {
+        ret.push(i);
+    }
+
+    return ret;
+};
+
+test("fizzbuzz", () => {
+    const ns = range(100).map((x) => x + 1);
+
+    return ns.map((n) => `${n}: ${fizzbuzz(n)}`).join("\n");
 });
