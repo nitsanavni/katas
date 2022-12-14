1f3593a4e44927f21254f9983b6405fa924104ce
Nitsan Avni <nitsanav@gmail.com>
2022-12-14 16:56:44 +0000

    stdin is difficult

--- verify-cli-bun/verify-cli-test.ts
+++ verify-cli-bun/verify-cli-test.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env bun
 
-import { sleepSync, spawn, spawnSync } from "bun";
+import { spawn, spawnSync } from "bun";
 
 const log = console.log;
 
@@ -16,37 +16,51 @@ const test = async (name: string, cb: () => void) => {
 // it triggers the provided diff tool
 // it supports passing in the test name, chnges behavior
 
-await test('says "test passed"', () => {
-    const { stdout } = spawnSync(["./verify.ts", "hello"], {
-        stdin: new TextEncoder().encode("hello"),
+// await test('says "test passed"', () => {
+//     const { stdout } = spawnSync(["./verify.ts", "test1"], {
+//         stdin: new TextEncoder().encode("test 1"),
+//     });
+//     log(stdout!.toString());
+// });
+
+// await test("get childs with pstree", async () => {
+//     const { pid } = spawn(["sh", "-c", "sleep 1"]);
+//     const shPid = String(pid);
+
+//     const { stdout: pstreeOut } = spawnSync(["pstree", "-a", shPid]);
+
+//     log({ pstree: pstreeOut?.toString().trim().split("\n") });
+
+//     spawnSync(["kill", shPid], { stderr: Bun.stderr });
+// });
+
+// await test('says "test failed"', () => {
+//     const { stdout } = spawnSync(["./verify.ts", "test2"], {
+//         stdin: new TextEncoder().encode("not test 2"),
+//     });
+//     log("outputs:", stdout!.toString());
+// });
+
+// await test("reads cli args: --test-arg", () => {
+//     const { stdout } = spawnSync(
+//         ["./verify.ts", "test3", "--test-arg", "my-value"],
+//         {
+//             stdin: new TextEncoder().encode("test 3"),
+//         }
+//     );
+//     log("outputs:", stdout!.toString());
+// });
+
+// await test("reads cli args: --echo", () => {
+//     const { stdout } = spawnSync(["./verify.ts", "test4", "--echo"], {
+//         stdin: new TextEncoder().encode("test 4"),
+//     });
+//     log("outputs:", stdout!.toString());
+// });
+
+await test("different test name", () => {
+    const { stdout } = spawnSync(["./verify.ts", "test5"], {
+        stdin: new TextEncoder().encode("hello 5!"),
     });
-    log(stdout!.toString());
-});
-
-await test("get childs with pstree", async () => {
-    const { pid } = spawn(["sh", "-c", "sleep 1"]);
-    const shPid = String(pid);
-
-    const { stdout: pstreeOut } = spawnSync(["pstree", "-a", shPid]);
-
-    log({ pstree: pstreeOut?.toString().trim().split("\n") });
-
-    spawnSync(["kill", shPid], { stderr: Bun.stderr });
-});
-
-await test('says "test failed"', () => {
-    const { stdout } = spawnSync(["./verify.ts", "hello"], {
-        stdin: new TextEncoder().encode("not hello"),
-    });
-    log("outputs:", stdout!.toString());
-});
-
-await test("reads cli args: --test-arg", () => {
-    const { stdout } = spawnSync(
-        ["./verify.ts", "hello", "--test-arg", "my-value"],
-        {
-            stdin: new TextEncoder().encode("hello"),
-        }
-    );
     log("outputs:", stdout!.toString());
 });
--- verify-cli-bun/verify.ts
+++ verify-cli-bun/verify.ts
@@ -1,11 +1,48 @@
 #!/usr/bin/env bun
+import { file, spawn, stdin, write } from "bun";
+import minimist from "minimist";
 
-let input = "";
+const argv = minimist(process.argv.slice(2));
+const test = argv._[0];
 
-for await (const line of console) {
-    input += line;
+if (argv["test-arg"]) {
+    console.log(argv["test-arg"]);
+    process.exit();
 }
 
-console.log(input == "hello" ? "test passed" : "test failed");
+process.stdin.end();
+
+let input = await stdin.text();
+
+// for await (const line of console) {
+//     // newline?
+//     input += line;
+// }
+
+if (argv["echo"]) {
+    console.log(input);
+    process.exit();
+}
+
+const received = `${test}.received`;
+const approved = `${test}.approved`;
+
+let approvedText;
+try {
+    approvedText = await file(approved).text();
+} catch {
+    await write(approved, "");
+}
+
+const match = input == approvedText;
+
+if (!match) {
+    console.log("test failed");
+    await write(received, input);
+    spawn(["code", "-d", received, approved]);
+    console.log("test failed 2");
+} else {
+    console.log("test passed");
+}
 
 export {};
