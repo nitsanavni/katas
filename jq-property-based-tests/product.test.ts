33a1f62350b09f62ce9f810cc4243b1fb24a7c23
Nitsan Avni <nitsanav@gmail.com>
2022-12-21 13:58:56 +0000

    failing fast-check test

--- jq-property-based-tests/product.test.ts
+++ jq-property-based-tests/product.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "bun:test";
+import fc from "fast-check";
 
-test("test the test", () => {
-    expect(true).toBe(false);
+test("test the fast-check", () => {
+    fc.assert(fc.property(fc.string(), (s) => s.length < 10));
 });
