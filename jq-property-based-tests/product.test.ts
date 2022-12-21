0eede4020c1a32caae6a59162c7bdf18b59c674c
Nitsan Avni <nitsanav@gmail.com>
2022-12-21 14:09:47 +0000

    spwan jaq from bun

--- jq-property-based-tests/product.test.ts
+++ jq-property-based-tests/product.test.ts
@@ -1,6 +1,11 @@
+import { file, spawn } from "bun";
 import { test, expect } from "bun:test";
 import fc from "fast-check";
 
-test("test the fast-check", () => {
+test("test the fast-check", async () => {
+    await spawn({
+        cmd: ["jaq", "-n", `${await file("product.jq").text()} 5 | square`],
+        stdout: "inherit",
+    });
     fc.assert(fc.property(fc.string(), (s) => s.length < 10));
 });
