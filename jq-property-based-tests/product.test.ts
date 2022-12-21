d8435870bdf783f0c1ab6e3b5e7138d9d5cb5548
Nitsan Avni <nitsanav@gmail.com>
2022-12-21 14:13:43 +0000

    thought

--- jq-property-based-tests/product.test.ts
+++ jq-property-based-tests/product.test.ts
@@ -1,8 +1,11 @@
+import { test } from "bun:test";
 import { file, spawn } from "bun";
-import { test, expect } from "bun:test";
 import fc from "fast-check";
 
-test("test the fast-check", async () => {
+// idea - to only launch jaq a few times (as opposed to hundreds of times),
+// we can use a fc.property with fc.array; invoking one jaq process for multiple inputs at once
+
+test("test jq code with fast-check", async () => {
     await spawn({
         cmd: ["jaq", "-n", `${await file("product.jq").text()} 5 | square`],
         stdout: "inherit",
