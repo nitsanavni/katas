67a1a61885ac78a8f0d203e04a0c73a6edc41a23
Nitsan Avni <nitsanav@gmail.com>
2022-12-29 19:01:22 +0000

    auto-commit

--- tcr-timer/fizzbuzz.test.ts
+++ tcr-timer/fizzbuzz.test.ts
@@ -1,15 +1,4 @@
 const fizzbuzz = (n: number) => {
-    if (n % 15 == 0) {
-        return "FizzBuzz";
-    }
-    if (n % 3 == 0) {
-        return "Fizz";
-    }
-
-    if (n % 5 == 0) {
-        return "Buzz";
-    }
-
     return String(n);
 };
 
