8c0633c511c93e314e56d77d9cd069eb56ba6298
Nitsan Avni <nitsanav@gmail.com>
2022-12-29 09:11:36 +0000

    good enough

--- bun-fizzbuzz/README.md
+++ bun-fizzbuzz/README.md
@@ -1,15 +1,5 @@
 # bun-fizzbuzz
 
-To install dependencies:
-
 ```bash
-bun install
+
 ```
-
-To run:
-
-```bash
-bun run fizzbuzz.test.ts
-```
-
-This project was created using `bun init` in bun v0.4.0. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- bun-fizzbuzz/fizzbuzz.test.ts
+++ bun-fizzbuzz/fizzbuzz.test.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+const specs = [
+    { divisor: 3, code: "Fizz" },
+    { divisor: 5, code: "Buzz" },
+    { divisor: 7, code: "Whizz" },
+];
+
+const fizzbuzz = (n: number): string => {
+    return (
+        specs
+            .filter(({ divisor }) => n % divisor == 0)
+            .map(({ code }) => code)
+            .join("") || String(n)
+    );
+};
+
+range(35)
+    .map((x) => x + 1)
+    .map(fizzbuzz)
+    .forEach((result) => console.log(result));
+
+function range(e: number): number[] {
+    return Array.from(Array(e)).map((_, i) => i);
+}
