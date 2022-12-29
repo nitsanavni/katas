a0bf9454f9a8a62f5b479d982c575d2f0a1a4790
Nitsan Avni <nitsanav@gmail.com>
2022-12-29 18:57:12 +0000

    auto-commit

--- tcr-timer/fizzbuzz.test.ts
+++ tcr-timer/fizzbuzz.test.ts
@@ -2,9 +2,7 @@ const fizzbuzz = (n: number) => String(n);
 
 range(1)
     .map((x) => x + 1)
-    .forEach((n) => {});
-console.log(fizzbuzz(1));
-
+    .forEach((n) => console.log(fizzbuzz(1)));
 function range(e: number) {
     return Array.from(Array(e)).map((_, i) => i);
 }
