cd436189d99a198d087733a18b5b8e97fafc7c7c
Nitsan Avni <nitsanav@gmail.com>
2022-12-29 09:31:59 +0000

    logging interference

--- bun-fizzbuzz/fizzbuzz.test.ts
+++ bun-fizzbuzz/fizzbuzz.test.ts
@@ -1,3 +1,9 @@
+import { debug as d } from "debug";
+const debug = d("fizzbuzz");
+
+// log debug statements to stdout, to intentionally interfere with our approval test
+debug.log = console.log.bind(console);
+
 const specs = [
     { divisor: 3, code: "Fizz" },
     { divisor: 5, code: "Buzz" },
@@ -8,6 +14,10 @@ const fizzbuzz = (n: number): string => {
     return (
         specs
             .filter(({ divisor }) => n % divisor == 0)
+            .filter((s) => {
+                debug({ ...s, n });
+                return true;
+            })
             .map(({ code }) => code)
             .join("") || String(n)
     );
@@ -15,8 +25,8 @@ const fizzbuzz = (n: number): string => {
 
 range(35)
     .map((x) => x + 1)
-    .map(fizzbuzz)
-    .forEach((result) => console.log(result));
+    .map((n) => [n, fizzbuzz(n)])
+    .forEach(([n, result]) => console.log(`! ${n} -> ${result}`));
 
 function range(e: number): number[] {
     return Array.from(Array(e)).map((_, i) => i);
