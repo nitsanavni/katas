6c605ea3adc18732ddcfd08af4656a98037ca0a7
Nitsan Avni <nitsanav@gmail.com>
2022-12-31 00:33:08 +0000

    experiments in testing

--- auto-rotate-by-moving-lines/test.sh
+++ auto-rotate-by-moving-lines/test.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/bash
 
 cat_input() {
-(cat << EOF
+cat << EOF
 $input
 EOF
-)
 }
-echo test 1
+
+
+echo test: rm typing indication
+
 input="Bob
 -- typing -->
 Alice"
 cat_input
-cat_input | jaq -sR '.'
+echo -----
+cat_input | jaq -sRr 'split("\n")[] | select(test("typing")|not)'
+echo ""
+
+
+echo test: move line down
+
+input="a
+b
+c"
+cat_input
+echo -----
+cat_input | jaq -sRr 'split("\n") | .[1] as $tmp | .[1] = .[2] | .[2] = $tmp | .[]'
+echo ""
+
+
+echo test: jq filter as bash var
+
+filter="range(5)
+| . * .
+| . + 1"
+jaq -nr "$filter"
 echo ""
