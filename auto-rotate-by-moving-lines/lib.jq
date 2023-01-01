2481bfe586ab696128b0cfa3c787e368b22f975b
Nitsan Avni <nitsanav@gmail.com>
2023-01-01 20:08:21 +0000

    find line by regex

--- auto-rotate-by-moving-lines/lib.jq
+++ auto-rotate-by-moving-lines/lib.jq
@@ -2,3 +2,12 @@ def swap(i; j):
     .[i] as $tmp |
     .[i] = .[j] |
     .[j] = $tmp;
+
+def index_of(re):
+    to_entries |
+    map(select(.value | test(re)) | .key) |
+    .[0];
+
+def move_line_down(re):
+    index_of("b") as $i |
+    swap($i; $i+1);
--- auto-rotate-by-moving-lines/test.sh
+++ auto-rotate-by-moving-lines/test.sh
@@ -14,22 +14,17 @@ input="Bob
 -- typing -->
 Alice"
 cat_input
-echo -----
+echo ------------------------
 cat_input | jaq -sRr 'split("\n")[] | select(test("typing")|not)'
 echo ""
 
 
 echo test: swap lines
-
-input="a
-b
-c"
-cat_input
-echo -----
+echo ------------------------
 echo 'swap(1;2)'
-cat_input | jaq -sRr "$lib"'split("\n") | swap(1;2) | .[]'
+jaq -sRr "$lib"'split("\n") | swap(1;2) | .[]' abc
 echo 'swap(0;2)'
-cat_input | jaq -sRr "$lib"'split("\n") | swap(0;2) | .[]'
+jaq -sRr "$lib"'split("\n") | swap(0;2) | .[]' abc
 echo ""
 
 
@@ -39,4 +34,14 @@ filter="range(5)
 | . * .
 | . + 1"
 jaq -nr "$filter"
+echo ""
+
+echo test: line index by regex
+echo ------------------------
+jaq -sRr "$lib"'split("\n") | index_of("b")' abc
+echo ""
+
+echo test: move regexed line one line down
+echo ------------------------
+jaq -sRr "$lib"'split("\n") | move_line_down("b") | .[]' abc
 echo ""
