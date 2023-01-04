a7373b83e6da590a2d4a67799152f25d48b0c1ca
Nitsan Avni <nitsanav@gmail.com>
2023-01-04 18:02:37 +0000

    coverage

--- gilded-rose-c/test.c
+++ gilded-rose-c/test.c
@@ -6,5 +6,5 @@ int main() {
   Item item;
   init_item(&item, "name", 0, 0);
 
-  printf("hello %s\n", item.name);
+  printf("%s %d %d\n", item.name, item.sellIn, item.quality);
 }
--- gilded-rose-c/test.sh
+++ gilded-rose-c/test.sh
@@ -1,2 +1,8 @@
-cc test.c GildedRose.c
+# build
+cc -g -Wall -Wextra -ftest-coverage -fprofile-arcs test.c GildedRose.c
+
+# verify
 ./a.out | bash verify.sh -t gilded-rose
+
+# coverage
+gcov -b GildedRose.c | grep executed
