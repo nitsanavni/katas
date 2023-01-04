a6cb0f4630c54c07a2edfa1b25a9c0f35d5aec72
Nitsan Avni <nitsanav@gmail.com>
2023-01-04 18:34:33 +0000

    "full coverage"

--- gilded-rose-c/GildedRose.c
+++ gilded-rose-c/GildedRose.c
@@ -1,98 +1,73 @@
-#include <string.h>
 #include "GildedRose.h"
 #include <stdio.h>
+#include <string.h>
 
-Item*
-init_item(Item* item, const char *name, int sellIn, int quality)
-{
-    item->sellIn = sellIn;
-    item->quality = quality;
-    item->name = strdup(name);
-    
-    return item;
+Item *init_item(Item *item, const char *name, int sellIn, int quality) {
+  item->sellIn = sellIn;
+  item->quality = quality;
+  item->name = strdup(name);
+
+  return item;
 }
 
-extern char* 
-print_item(char* buffer, Item* item) 
-{
-    sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
+extern char *print_item(char *buffer, Item *item) {
+  sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
+  return buffer;
 }
 
-void 
-update_quality(Item items[], int size) 
-{
-    int i;
-    
-    for (i = 0; i < size; i++)
-    {
-        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-        {
-            if (items[i].quality > 0)
-            {
-                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        }
-        else
-        {
-            if (items[i].quality < 50)
-            {
-                items[i].quality = items[i].quality + 1;
+void update_quality(Item items[], int size) {
+  int i;
 
-                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
+  for (i = 0; i < size; i++) {
+    if (strcmp(items[i].name, "Aged Brie") &&
+        strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert")) {
+      if (items[i].quality > 0) {
+        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
+          items[i].quality = items[i].quality - 1;
         }
+      }
+    } else {
+      if (items[i].quality < 50) {
+        items[i].quality = items[i].quality + 1;
 
-        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-        {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
+        if (!strcmp(items[i].name,
+                    "Backstage passes to a TAFKAL80ETC concert")) {
+          if (items[i].sellIn < 11) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
+            }
+          }
 
-        if (items[i].sellIn < 0)
-        {
-            if (strcmp(items[i].name, "Aged Brie"))
-            {
-                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
+          if (items[i].sellIn < 6) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
             }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
-                }
-            }
+          }
         }
+      }
     }
+
+    if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
+      items[i].sellIn = items[i].sellIn - 1;
+    }
+
+    if (items[i].sellIn < 0) {
+      if (strcmp(items[i].name, "Aged Brie")) {
+        if (strcmp(items[i].name,
+                   "Backstage passes to a TAFKAL80ETC concert")) {
+          if (items[i].quality > 0) {
+            if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros")) {
+              items[i].quality = items[i].quality - 1;
+            }
+          }
+        } else {
+          items[i].quality = items[i].quality - items[i].quality;
+        }
+      } else {
+        if (items[i].quality < 50) {
+          items[i].quality = items[i].quality + 1;
+        }
+      }
+    }
+  }
 }
--- gilded-rose-c/test.c
+++ gilded-rose-c/test.c
@@ -7,21 +7,24 @@ int main() {
   update_quality(&item, 0);
 
   const char *names[] = {"name", "Aged Brie",
-                         "Backstage passes to a TAFKAL80ETC concert"};
-  const int sellIns[] = {0};
-  const int qualities[] = {0};
+                         "Backstage passes to a TAFKAL80ETC concert",
+                         "Sulfuras, Hand of Ragnaros"};
+  const int sellIns[] = {-1, 0, 11};
+  const int qualities[] = {0, 1, 49, 50};
 
-  for (const char **name = names; name < names + 3; name++) {
+  for (const char **name = names; name < names + 4; name++) {
     printf("%s\n", *name);
 
-    for (const int *sellIn = sellIns; sellIn < sellIns + 1; sellIn++) {
-      for (const int *quality = qualities; quality < qualities + 1; quality++) {
-        init_item(&item, *name, *sellIn, 0);
+    for (const int *sellIn = sellIns; sellIn < sellIns + 3; sellIn++) {
+      for (const int *quality = qualities; quality < qualities + 4; quality++) {
+        init_item(&item, *name, *sellIn, *quality);
 
         printf("%d %d -> ", item.sellIn, item.quality);
         update_quality(&item, 1);
         printf("%d %d\n", item.sellIn, item.quality);
       }
     }
+
+    printf("\n");
   }
 }
