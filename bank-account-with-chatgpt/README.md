37e896edc6cca6819c70edc6954fe2bb127e8276
Nitsan Avni <nitsanav@gmail.com>
2023-01-07 20:03:17 +0000

    1st test

---
+++ bank-account-with-chatgpt/README.md
@@ -0,0 +1,5 @@
+## run the tests
+
+```shell
+python test_bank_account.py | bash verify.sh -t my-test
+```
---
+++ bank-account-with-chatgpt/test_bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def balance(self):
+        pass
+
+
+account = BankAccount()
+balance = account.balance()
+print("Test: check balance of a new account")
+print(f"The balance of the bank account is\n{balance}")
