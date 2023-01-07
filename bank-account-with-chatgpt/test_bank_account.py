2dd27e825d79096aa23243ca103534d7045d66c3
Nitsan Avni <nitsanav@gmail.com>
2023-01-07 21:07:20 +0000

    aligning cols

--- bank-account-with-chatgpt/test_bank_account.py
+++ bank-account-with-chatgpt/test_bank_account.py
@@ -5,30 +5,27 @@ class BankAccount:
     def deposit(self, amount):
         self.transactions.append(amount)
 
+    def withdraw(self, amount):
+        self.transactions.append(-amount)
+
     def print_statement(self):
-        print("Date       || Amount || Balance")
         balance = 0
-        for t in self.transactions:
-            balance += t
-            print("           ||    %5d    ||    %5d" % (t, balance))
+        print(f"{'Date':>10} || {'Amount':>7} || {'Balance':>7}")
+        for transaction in self.transactions:
+            balance += transaction
+            print(f"{' ':>10} || {transaction:>7} || {balance:>7}")
 
 
 print("Test: print statement of a new account")
-
 account = BankAccount()
-
 account.print_statement()
 
 print("\nTest: print statement after single deposit")
-
 account.deposit(1000)
-
 account.print_statement()
 
 print("\nTest: print statement after multiple deposits")
-
 account.deposit(1000)
 account.deposit(2000)
-account.deposit(-500)
-
+account.withdraw(500)
 account.print_statement()
