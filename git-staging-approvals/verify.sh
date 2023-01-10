35a4995ef43dac1191bff18ab63af720a20907df
Nitsan Avni <nitsanav@gmail.com>
2023-01-10 21:12:23 +0000

    styling

--- git-staging-approvals/verify.sh
+++ git-staging-approvals/verify.sh
@@ -7,15 +7,11 @@ git add --intent-to-add result
 git diff --exit-code result > /dev/null \
     && \
         ( \
-            echo "test passed"; \
+            echo "*** test passed ***"; \
         ) \
     || \
         ( \
-            echo ""; \
-            echo ""; \
             echo "*** test failed ***"; \
-            echo ""; \
-            echo ""; \
             bash -c "git add -p result"; \
             false \
         )
