3adfae13253ad9a50134e2a81c1a3888f94510ea
Nitsan Avni <nitsanav@gmail.com>
2023-01-26 20:54:36 +0000

    gilded rose in C: automated pairwise tests

--- gilded-rose-c/test.sh
+++ gilded-rose-c/test.sh
@@ -1,3 +1,6 @@
+# jenny
+../jenny/jenny -n2 4 3 4 | (echo const int jenny[][3] = {; sed -f indices-jenny.sed;echo }\;;) > jenny.h
+
 # build
 cc -g -Wall -Wextra -ftest-coverage -fprofile-arcs test.c GildedRose.c
 # cc -g -Wall -Wextra test.c GildedRose.c
