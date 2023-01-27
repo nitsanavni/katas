3297f91218afea98c01d83733c1f5039665db690
Nitsan Avni <nitsanav@gmail.com>
2023-01-27 07:36:00 +0000

    test.sh: organize

--- gilded-rose-c/test.sh
+++ gilded-rose-c/test.sh
@@ -1,13 +1,17 @@
-# jenny
+echo --- jenny ---
 ../jenny/jenny -n2 4 3 4 | (echo const int jenny[][3] = {; sed -f indices-jenny.sed;echo }\;;) > jenny.h
+echo ""
 
-# build
+echo --- build ---
 cc -g -Wall -Wextra -ftest-coverage -fprofile-arcs test.c GildedRose.c
 # cc -g -Wall -Wextra test.c GildedRose.c
+echo ""
 
-# verify
+echo --- verify ---
 ./a.out | bash verify.sh -t gilded-rose
+echo ""
 
-# coverage
+echo --- coverage ---
 gcov -bk GildedRose.c
-less -r GildedRose.c.gcov
+# less -r GildedRose.c.gcov
+echo ""
