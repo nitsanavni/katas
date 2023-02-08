b11ccb34b274d00aaa4a7c2cdae2b16a8b857d9a
Nitsan Avni <nitsanav@gmail.com>
2023-02-08 20:10:08 +0000

    inline approvals with doctest

--- inline-approvals-with-doctest/README.md
+++ inline-approvals-with-doctest/README.md
@@ -1,3 +1,7 @@
 ```
+ls fractions.py | entr -sc 'pytest --doctest-modules fractions.py --accept-copy || code --diff fractions.py.new fractions.py'
+```
+
+```
 ls fractions.py | entr -c pytest --doctest-modules fractions.py --accept-copy
 ```
--- inline-approvals-with-doctest/fractions.py
+++ inline-approvals-with-doctest/fractions.py
@@ -4,32 +4,59 @@ class Fraction:
         self.nominator = nominator
         self.denominator = denominator
 
+    def __str__(self):
+        return self.format()
+
     def format(self):
-        return f"{f'{self.whole} ' if self.whole != 0 else ''}{f'{self.nominator}/{self.denominator}' if self.nominator != 0 else ''}"
+        if self.whole != 0:
+            if self.nominator != 0:
+                return f"{self.whole} {self.nominator}/{self.denominator}"
+            return f"{self.whole}"
+        return f"{self.nominator}/{self.denominator}"
+
+    def add(self, other):
+        return Fraction(
+            whole=self.whole + other.whole,
+            nominator=self.nominator * other.denominator +
+            other.nominator * self.denominator,
+            denominator=self.denominator * other.denominator
+        )
 
 
 one_and_a_half = Fraction(whole=1, nominator=1, denominator=2)
 one = Fraction(whole=1)
 one_half = Fraction(nominator=1, denominator=2)
+fractions = [
+    ('one and a half', one_and_a_half),
+    ('one', one),
+    ('one half', one_half),
+]
+combinations = [(f1, f2) for n1, f1 in fractions for n2, f2 in fractions]
 
 
-def t_format():
+def verify_add():
     """
-    >>> t_format()
+    >>> verify_add()
+    1 1/2 + 1 1/2 = 2 4/4
+    1 1/2 + 1 = 2 1/2
+    1 1/2 + 1/2 = 1 4/4
+    1 + 1 1/2 = 2 1/2
+    1 + 1 = 2
+    1 + 1/2 = 1 1/2
+    1/2 + 1 1/2 = 1 4/4
+    1/2 + 1 = 1 1/2
+    1/2 + 1/2 = 4/4
+    """
+    for f1, f2 in combinations:
+        print(f"{f1} + {f2} = {f1.add(f2)}")
+
+
+def verify_format():
+    """
+    >>> verify_format()
     one and a half 1 1/2
-    one 1 
+    one 1
     one half 1/2
     """
-    fractions = [
-        ['one and a half', one_and_a_half],
-        ['one', one],
-        ['one half', one_half],
-    ]
     for name, f in fractions:
         print(name, f.format())
-
-
-def fail_to_always_regenerates_the_diff():
-    """
-    >>> 1
-    """
