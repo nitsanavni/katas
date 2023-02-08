c3b2a52c91849b9039c085dc4d76384482e3a481
Nitsan Avni <nitsanav@gmail.com>
2023-02-08 21:18:52 +0000

    extract classes

--- parrot/parrot.py
+++ parrot/parrot.py
@@ -1,7 +1,7 @@
-from enum import Enum  # Enum is introduced in Python 3.4.
+from enum import Enum
 
 
-class ParrotType(Enum):  # If it is not available, just remove it.
+class ParrotType(Enum):
     EUROPEAN = 1
     AFRICAN = 2
     NORWEGIAN_BLUE = 3
@@ -10,29 +10,48 @@ class ParrotType(Enum):  # If it is not available, just remove it.
 class Parrot:
 
     def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
-        self._type = type_of_parrot
-        self._number_of_coconuts = number_of_coconuts
-        self._voltage = voltage
-        self._nailed = nailed
+        self._parrot = [EuropeanParrot, AfricanParrot, NorwegianBlueParrot][type_of_parrot.value - 1](
+            type_of_parrot=type_of_parrot, number_of_coconuts=number_of_coconuts, voltage=voltage, nailed=nailed)
 
     def speed(self):
-        if self._type == ParrotType.EUROPEAN:
-            return self._base_speed()
-        if self._type == ParrotType.AFRICAN:
-            return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
-        if self._type == ParrotType.NORWEGIAN_BLUE:
-            if self._nailed:
-                return 0
-            else:
-                return self._compute_base_speed_for_voltage(self._voltage)
+        return self._parrot.speed()
 
-        raise ValueError("should be unreachable")
+    def _base_speed(self):
+        return 12.0
 
-    def _compute_base_speed_for_voltage(self, voltage):
-        return min([24.0, voltage * self._base_speed()])
+
+class EuropeanParrot(Parrot):
+
+    def __init__(self, **k):
+        pass
+
+    def speed(self):
+        return self._base_speed()
+
+
+class AfricanParrot(Parrot):
+
+    def __init__(self, *, number_of_coconuts, **k):
+        self._number_of_coconuts = number_of_coconuts
+
+    def speed(self):
+        return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
 
     def _load_factor(self):
         return 9.0
 
-    def _base_speed(self):
-        return 12.0
+
+class NorwegianBlueParrot(Parrot):
+
+    def __init__(self, *, voltage, nailed, **k):
+        self._voltage = voltage
+        self._nailed = nailed
+
+    def speed(self):
+        if self._nailed:
+            return 0
+        else:
+            return self._compute_base_speed_for_voltage()
+
+    def _compute_base_speed_for_voltage(self):
+        return min(24.0, self._voltage * self._base_speed())
