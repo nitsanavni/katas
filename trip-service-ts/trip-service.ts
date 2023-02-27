dba14e19c4b971147bff26745fc12594b5d8cd8b
Nitsan Avni <nitsanav@gmail.com>
2023-02-27 02:20:15 +0000

    :/

--- trip-service-ts/trip-service.ts
+++ trip-service-ts/trip-service.ts
@@ -36,6 +36,9 @@ export default class TripDAO {
     }
 }
 class UserNotLoggedInException extends Error {}
+const throwUserNotLoggedIn = () => {
+    throw new UserNotLoggedInException();
+};
 class TripService {
     public getTripsByUser(user: User): Trip[] {
         let tripList: Trip[] = [];
@@ -56,6 +59,7 @@ class TripService {
 
             return tripList;
         } else {
+            throwUserNotLoggedIn();
             throw new UserNotLoggedInException();
         }
     }
@@ -63,7 +67,54 @@ class TripService {
 
 import { stub } from "sinon";
 
-console.log("test");
+console.log("test 1");
 const service = new TripService();
 stub(UserSession, "getLoggedUser");
-service.getTripsByUser(null);
+stub(TripDAO, "findTripsByUser");
+try {
+    service.getTripsByUser(null);
+} catch (e) {
+    console.log(e.constructor.name);
+}
+UserSession.getLoggedUser.restore();
+
+console.log("test 2");
+stub(UserSession, "getLoggedUser").callsFake(() => new User());
+console.log(service.getTripsByUser(new User()));
+UserSession.getLoggedUser.restore();
+
+console.log("test 3");
+{
+    const u = new User();
+    const f = new User();
+    u.addFriend(f);
+    f.addFriend(u);
+    stub(UserSession, "getLoggedUser").callsFake(() => u);
+    console.log(service.getTripsByUser(f));
+    UserSession.getLoggedUser.restore();
+}
+console.log("test 3");
+{
+    const u = new User();
+    const f = new User();
+    u.addFriend(f);
+    f.addFriend(u);
+    stub(UserSession, "getLoggedUser").callsFake(() => u);
+    console.log(service.getTripsByUser(u));
+    UserSession.getLoggedUser.restore();
+}
+
+console.log("test 4");
+{
+    const u = new User();
+    const f = new User();
+    u.addFriend(f);
+    u.addTrip(new Trip());
+    f.addFriend(u);
+    f.addTrip(new Trip());
+    stub(UserSession, "getLoggedUser").callsFake(() => u);
+    console.log(service.getTripsByUser(u));
+    console.log(u.getTrips());
+    console.log(f.getTrips());
+    UserSession.getLoggedUser.restore();
+}
