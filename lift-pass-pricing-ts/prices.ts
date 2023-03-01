b13c5f4977854e79f4a421fcc073f34b68efbc08
Nitsan Avni <nitsanav@gmail.com>
2023-03-01 15:50:44 +0000

    extract `logic` - to test w/o network

--- lift-pass-pricing-ts/prices.ts
+++ lift-pass-pricing-ts/prices.ts
@@ -2,6 +2,78 @@
 import express from "express";
 import mysql from "mysql2/promise";
 
+const logic =
+    (connection: mysql.Connection) =>
+    async ({ type, age, date }: any) => {
+        const q = await connection.query(
+            "SELECT cost FROM `base_price` " + "WHERE `type` = ? ",
+            [type]
+        );
+
+        // console.log(inspect(q));
+        // @ts-ignore
+        const result = q[0][0];
+
+        if ((age as any) < 6) {
+            return { cost: 0 };
+        } else {
+            if (type !== "night") {
+                const holidays = (
+                    await connection.query("SELECT * FROM `holidays`")
+                )[0] as mysql.RowDataPacket[];
+
+                let isHoliday;
+                let reduction = 0;
+                for (let row of holidays) {
+                    let holiday = row.holiday;
+                    if (date) {
+                        let d = new Date(date as string);
+                        if (
+                            d.getFullYear() === holiday.getFullYear() &&
+                            d.getMonth() === holiday.getMonth() &&
+                            d.getDate() === holiday.getDate()
+                        ) {
+                            isHoliday = true;
+                        }
+                    }
+                }
+
+                if (!isHoliday && new Date(date as string).getDay() === 1) {
+                    reduction = 35;
+                }
+
+                // TODO apply reduction for others
+                if ((age as any) < 15) {
+                    return { cost: Math.ceil(result.cost * 0.7) };
+                } else {
+                    if (age === undefined) {
+                        let cost = result.cost * (1 - reduction / 100);
+                        return { cost: Math.ceil(cost) };
+                    } else {
+                        if ((age as any) > 64) {
+                            let cost =
+                                result.cost * 0.75 * (1 - reduction / 100);
+                            return { cost: Math.ceil(cost) };
+                        } else {
+                            let cost = result.cost * (1 - reduction / 100);
+                            return { cost: Math.ceil(cost) };
+                        }
+                    }
+                }
+            } else {
+                if ((age as any) >= 6) {
+                    if ((age as any) > 64) {
+                        return { cost: Math.ceil(result.cost * 0.4) };
+                    } else {
+                        return result;
+                    }
+                } else {
+                    return { cost: 0 };
+                }
+            }
+        }
+    };
+
 async function createApp() {
     const app = express();
 
@@ -25,77 +97,7 @@ async function createApp() {
         res.json();
     });
     app.get("/prices", async (req, res) => {
-        console.log(req.query);
-        const q = await connection.query(
-            "SELECT cost FROM `base_price` " + "WHERE `type` = ? ",
-            [req.query.type]
-        );
-
-        // console.log(inspect(q));
-        // @ts-ignore
-        const result = q[0][0];
-
-        if ((req.query.age as any) < 6) {
-            res.json({ cost: 0 });
-        } else {
-            if (req.query.type !== "night") {
-                const holidays = (
-                    await connection.query("SELECT * FROM `holidays`")
-                )[0] as mysql.RowDataPacket[];
-
-                let isHoliday;
-                let reduction = 0;
-                for (let row of holidays) {
-                    let holiday = row.holiday;
-                    if (req.query.date) {
-                        let d = new Date(req.query.date as string);
-                        if (
-                            d.getFullYear() === holiday.getFullYear() &&
-                            d.getMonth() === holiday.getMonth() &&
-                            d.getDate() === holiday.getDate()
-                        ) {
-                            isHoliday = true;
-                        }
-                    }
-                }
-
-                if (
-                    !isHoliday &&
-                    new Date(req.query.date as string).getDay() === 1
-                ) {
-                    reduction = 35;
-                }
-
-                // TODO apply reduction for others
-                if ((req.query.age as any) < 15) {
-                    res.json({ cost: Math.ceil(result.cost * 0.7) });
-                } else {
-                    if (req.query.age === undefined) {
-                        let cost = result.cost * (1 - reduction / 100);
-                        res.json({ cost: Math.ceil(cost) });
-                    } else {
-                        if ((req.query.age as any) > 64) {
-                            let cost =
-                                result.cost * 0.75 * (1 - reduction / 100);
-                            res.json({ cost: Math.ceil(cost) });
-                        } else {
-                            let cost = result.cost * (1 - reduction / 100);
-                            res.json({ cost: Math.ceil(cost) });
-                        }
-                    }
-                }
-            } else {
-                if ((req.query.age as any) >= 6) {
-                    if ((req.query.age as any) > 64) {
-                        res.json({ cost: Math.ceil(result.cost * 0.4) });
-                    } else {
-                        res.json(result);
-                    }
-                } else {
-                    res.json({ cost: 0 });
-                }
-            }
-        }
+        res.json(await logic(connection)(req.query));
     });
     return { app, connection };
 }
