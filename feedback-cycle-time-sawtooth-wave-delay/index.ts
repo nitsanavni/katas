85258638fdf075f677dd3ebec26e9de522ceff2d
Nitsan Avni <nitsanav@gmail.com>
2023-03-04 07:29:47 +0000

    impl delay

--- feedback-cycle-time-sawtooth-wave-delay/index.ts
+++ feedback-cycle-time-sawtooth-wave-delay/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { sleep } from "bun";
+
+const delay = (ms: number) => {
+    const two_minutes = 2 * 60 * 1000;
+    const max_delay = 20 * 1000;
+    const min_delay = 0;
+
+    const result =
+        ((ms % two_minutes) * (min_delay - max_delay)) / two_minutes +
+        max_delay;
+
+    console.error(Math.floor(result / 1000));
+    return result;
+};
+
+await sleep(delay(Date.now()));
