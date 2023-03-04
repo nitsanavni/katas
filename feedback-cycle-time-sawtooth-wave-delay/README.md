26b01a40447eb996936aae3097f1d60334eae491
Nitsan Avni <nitsanav@gmail.com>
2023-03-04 08:17:51 +0000

    impl fizzbuzz and thoughts

--- feedback-cycle-time-sawtooth-wave-delay/README.md
+++ feedback-cycle-time-sawtooth-wave-delay/README.md
@@ -8,3 +8,14 @@ to get a feel of the importance of a quick feedback cycle - introduce varying ar
 bun upgrade
 bun install
 ```
+
+# develop
+
+```shell
+ls index.ts approved | entr -cr ./test.sh
+```
+
+# thoughts
+
+-   sin wave delay
+-   in a pair / ensemble - we could set the timer of the rotation to be different than the delay cycle time; e.g. ensemble rotation every 3m delay cycle 4m
--- feedback-cycle-time-sawtooth-wave-delay/index.ts
+++ feedback-cycle-time-sawtooth-wave-delay/index.ts
@@ -1,16 +1,32 @@
 import { sleep } from "bun";
 
-const delay = (ms: number) => {
-    const two_minutes = 2 * 60 * 1000;
+const sinDelay = (ms: number) => {
+    const cycle = 4 * 60 * 1000;
     const max_delay = 20 * 1000;
     const min_delay = 0;
 
-    const result =
-        ((ms % two_minutes) * (min_delay - max_delay)) / two_minutes +
-        max_delay;
+    const s = Math.sin((2 * Math.PI * ms) / cycle);
 
+    const result = ((s + 1) * (min_delay - max_delay)) / 2 + max_delay;
+
+    console.error(s);
     console.error(Math.floor(result / 1000));
     return result;
 };
 
-await sleep(delay(Date.now()));
+await sleep(sinDelay(Date.now()));
+
+console.log("fizzbuzz");
+const part = (d: number, code: string) => (n: number) => n % d == 0 ? code : "";
+const specs = [
+    [3, "Fizz"],
+    [5, "Buzz"],
+    [7, "Whizz"],
+    [11, "Bang"],
+] as [number, string][];
+
+const fizzbuzz = (n: number) => specs.map((p) => part(...p)(n)).join("") || n;
+
+for (let n = 1; n <= 35; n++) {
+    console.log(`${n} -> ${fizzbuzz(n)}`);
+}
