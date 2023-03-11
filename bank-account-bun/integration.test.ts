79a48b989f768d8c4d8bf015e42f247e7a6a25c5
Nitsan Avni <nitsanav@gmail.com>
2023-03-11 07:30:35 +0000

    minor

--- bank-account-bun/integration.test.ts
+++ bank-account-bun/integration.test.ts
@@ -2,10 +2,12 @@ import { describe } from "bun:test";
 import { test } from "./approvals.js";
 import { driver } from "./bank-account.driver.js";
 
+const dateRe = /\d{4}-\d{2}-\d{2}/g;
+
 describe("integration test (real dates, and stdout)", () => {
     test("integration", async () =>
         (await driver([1000, 100, 10, 1, -2, -30, -400, -5000])).replaceAll(
-            /\d{4}-\d{2}-\d{2}/g,
+            dateRe,
             "<scrubbed>"
         ));
 });
