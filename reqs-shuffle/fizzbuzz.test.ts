12165ef9868b7dfbe766c4d90497406522804de1
Nitsan Avni <nitsanav@gmail.com>
2023-03-15 07:00:30 +0000

    fizz

--- reqs-shuffle/fizzbuzz.test.ts
+++ reqs-shuffle/fizzbuzz.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "bun:test";
-import { makeCounter, makeFizzbuzz, pure } from "./fizzbuzz.js";
+
+import { makeCounter, makeFizzbuzz, pureFizzbuzz } from "./fizzbuzz.js";
 
 test("counter", () => {
     const counter = makeCounter();
@@ -9,20 +10,24 @@ test("counter", () => {
     expect(counter()).toEqual(2);
 });
 
+test("if n is a multiple of 3 result is Fizz", () => {
+    expect(pureFizzbuzz(3)).toEqual("Fizz");
+    expect(pureFizzbuzz(72)).toEqual("Fizz");
+});
 test("if n contains the digit `7` result is Whizz", () => {
-    expect(pure(7)).toEqual("Whizz");
-    expect(pure(17)).toEqual("Whizz");
-    expect(pure(76)).toEqual("Whizz");
+    expect(pureFizzbuzz(7)).toEqual("Whizz");
+    expect(pureFizzbuzz(17)).toEqual("Whizz");
+    expect(pureFizzbuzz(76)).toEqual("Whizz");
 });
 
 test("result is a string or number", () => {
-    expect(pure(7)).toEqual("Whizz");
-    expect(pure(8)).toEqual(8);
+    expect(pureFizzbuzz(7)).toEqual("Whizz");
+    expect(pureFizzbuzz(8)).toEqual(8);
 });
 
 test("if n is a multiple of 7 result is Whizz", () => {
-    expect(pure(7)).toEqual("Whizz");
-    expect(pure(14)).toEqual("Whizz");
+    expect(pureFizzbuzz(7)).toEqual("Whizz");
+    expect(pureFizzbuzz(14)).toEqual("Whizz");
 });
 
 test("returns the result", () => {
@@ -38,7 +43,7 @@ test.skip("prints the result to stdout", async () => {
 test("tracks the number of calls internally", () => {
     const fb = makeFizzbuzz();
 
-    expect(fb()).toEqual(1);
-    expect(fb()).toEqual(2);
-    expect(fb()).toEqual(3);
+    expect(fb()).toEqual(pureFizzbuzz(1));
+    expect(fb()).toEqual(pureFizzbuzz(2));
+    expect(fb()).toEqual(pureFizzbuzz(3));
 });
--- reqs-shuffle/fizzbuzz.ts
+++ reqs-shuffle/fizzbuzz.ts
@@ -3,13 +3,19 @@ export const makeCounter =
     () =>
         i++;
 
-export const pure = (n: number) =>
-    n % 7 == 0 ? "Whizz" : /7/.test(String(n)) ? "Whizz" : n;
+export const pureFizzbuzz = (n: number) =>
+    n % 3 == 0
+        ? "Fizz"
+        : n % 7 == 0
+        ? "Whizz"
+        : /7/.test(String(n))
+        ? "Whizz"
+        : n;
 
 export const makeFizzbuzz = () => {
     const counter = makeCounter(1);
 
     return () => {
-        return pure(counter());
+        return pureFizzbuzz(counter());
     };
 };
