4abcf24f9ef4e958d58c7462db72b072a8486f82
Nitsan Avni <nitsanav@gmail.com>
2023-03-15 07:05:35 +0000

    new req: fb is pure

--- reqs-shuffle/fizzbuzz.test.ts
+++ reqs-shuffle/fizzbuzz.test.ts
@@ -1,6 +1,10 @@
 import { test, expect } from "bun:test";
 
-import { makeCounter, makeFizzbuzz, pureFizzbuzz } from "./fizzbuzz.js";
+import {
+    makeCounter,
+    makeFizzbuzz as selfCountingFizzbuzz,
+    pureFizzbuzz as fizzbuzz,
+} from "./fizzbuzz.js";
 
 test("counter", () => {
     const counter = makeCounter();
@@ -11,27 +15,27 @@ test("counter", () => {
 });
 
 test("if n is a multiple of 3 result is Fizz", () => {
-    expect(pureFizzbuzz(3)).toEqual("Fizz");
-    expect(pureFizzbuzz(72)).toEqual("Fizz");
+    expect(fizzbuzz(3)).toEqual("Fizz");
+    expect(fizzbuzz(72)).toEqual("Fizz");
 });
 test("if n contains the digit `7` result is Whizz", () => {
-    expect(pureFizzbuzz(7)).toEqual("Whizz");
-    expect(pureFizzbuzz(17)).toEqual("Whizz");
-    expect(pureFizzbuzz(76)).toEqual("Whizz");
+    expect(fizzbuzz(7)).toEqual("Whizz");
+    expect(fizzbuzz(17)).toEqual("Whizz");
+    expect(fizzbuzz(76)).toEqual("Whizz");
 });
 
 test("result is a string or number", () => {
-    expect(pureFizzbuzz(7)).toEqual("Whizz");
-    expect(pureFizzbuzz(8)).toEqual(8);
+    expect(fizzbuzz(7)).toEqual("Whizz");
+    expect(fizzbuzz(8)).toEqual(8);
 });
 
 test("if n is a multiple of 7 result is Whizz", () => {
-    expect(pureFizzbuzz(7)).toEqual("Whizz");
-    expect(pureFizzbuzz(14)).toEqual("Whizz");
+    expect(fizzbuzz(7)).toEqual("Whizz");
+    expect(fizzbuzz(14)).toEqual("Whizz");
 });
 
 test("returns the result", () => {
-    const fb = makeFizzbuzz();
+    const fb = selfCountingFizzbuzz();
 
     expect(fb()).toEqual(1);
 });
@@ -40,10 +44,10 @@ test.skip("prints the result to stdout", async () => {
     // I need a driver
 });
 
-test("tracks the number of calls internally", () => {
-    const fb = makeFizzbuzz();
+test.skip("tracks the number of calls internally", () => {
+    const fb = selfCountingFizzbuzz();
 
-    expect(fb()).toEqual(pureFizzbuzz(1));
-    expect(fb()).toEqual(pureFizzbuzz(2));
-    expect(fb()).toEqual(pureFizzbuzz(3));
+    expect(fb()).toEqual(fizzbuzz(1));
+    expect(fb()).toEqual(fizzbuzz(2));
+    expect(fb()).toEqual(fizzbuzz(3));
 });
