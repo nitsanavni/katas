2b09c4a4af73a8d457add8f1ea2725da665d3540
Nitsan Avni <nitsanav@gmail.com>
2023-04-23 13:23:06 +0000

    r specs.filter iso specs.map(part)

--- fizzbuzz-delayed-refactoring/fizzbuzz.test.ts
+++ fizzbuzz-delayed-refactoring/fizzbuzz.test.ts
@@ -1,25 +1,31 @@
 import { EOL } from "os";
 import { test } from "./approvals";
 
+type Spec = [d: number, code: string];
+
 const specs = [
     [3, "Fizz"],
     [5, "Buzz"],
-] as const;
+] as Spec[];
 
 const part =
     ([d, code]: readonly [d: number, code: string]) =>
     (n: number) =>
         n % d == 0 ? code : "";
 
+const predicate =
+    (n: number) =>
+    ([d]: Spec) =>
+        n % d == 0;
+
+const code = ([_, code]: Spec): string => code;
+
 const fizzbuzz = (n: number) =>
-    specs
-        .map(part)
-        .map((f) => f(n))
-        .join("") || n;
+    specs.filter(predicate(n)).map(code).join("") || n;
 
 const range = (length = 10) => Array.from({ length }, (_, i) => i + 1);
 
 test("fizzbuzz", () =>
-    range(15)
+    range(35)
         .map((n) => `${n} -> ${fizzbuzz(n)}`)
         .join(EOL));
