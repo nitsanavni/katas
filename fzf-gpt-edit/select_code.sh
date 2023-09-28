f1fa8fb155e24f2e6c566a01469528b1ca3c6412
Nitsan Avni <nitsanav@gmail.com>
2023-09-28 14:13:58 +0000

    adds explain code briefly functionality to select_code.sh
    Co-Authored-By: Gregor Riegler <rieglerg85@gmail.com>
    Co-Authored-By: Michael R. Wolf <MichaelRWolf@att.net>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Tsvetan Tsvetanov <cpi.cecko@gmail.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>
    Co-Authored-By: Woody Zuill <wzuill@yahoo.com>

---
+++ fzf-gpt-edit/select_code.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Use fzf to select a file
+SELECTED_FILE=$(fzf --prompt="Select a file: ")
+
+# Check if a file was selected
+if [ -z "$SELECTED_FILE" ]; then
+    echo "No file selected."
+    exit 1
+fi
+
+# Display the selected file with line numbers and allow users to select the start
+START_LINE=$(cat "$SELECTED_FILE" -n | fzf --layout=reverse --prompt="Select start: " | awk '{print $1}')
+
+# Check if a start line was selected
+if [ -z "$START_LINE" ]; then
+    echo "No start line selected."
+    exit 1
+fi
+
+# Display the file from the start line onwards and allow users to select the end
+END_LINE=$(cat "$SELECTED_FILE" -n | tail -n +$START_LINE | fzf --layout=reverse --prompt="Select end: " | awk '{print $1}')
+
+# Check if an end line was selected
+if [ -z "$END_LINE" ]; then
+    echo "No end line selected."
+    exit 1
+fi
+
+# Display the selected range from the file
+SELECTED_CODE=$(sed -n "${START_LINE},${END_LINE}p" "$SELECTED_FILE")
+
+./chat "explain this code in a few words\n${SELECTED_CODE}"
