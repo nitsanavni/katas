09f943607a00335d17eef4926c923fe1309a2bc6
Nitsan Avni <nitsanav@gmail.com>
2023-10-01 07:09:10 +0000

    tiny files

--- utils/fizzbuzz_kata/test_fizzbuzz_of_four.py
+++ utils/fizzbuzz_kata/test_fizzbuzz_of_four.py
@@ -1,3 +1,4 @@
 from fizzbuzz import fizzbuzz
+
 def test_fizzbuzz_of_four():
     assert fizzbuzz(4) == 4
--- utils/prompt.sh
+++ utils/prompt.sh
@@ -6,6 +6,7 @@ follow the same style as the existing code
 run the tests as often as possible - just run `pytest` - before and after each change
 use TDD
 always read code before changing it
+don't attempt to read a file before checking it's there - `ls`
 work in tiny steps
 validate after each step
 change code by overwriting complete files, don't attempt to change code in place
