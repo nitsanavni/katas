d762af63e26772f16b9e52e71ecb26a5aec717d0
Nitsan Avni <nitsanav@gmail.com>
2023-10-01 07:05:10 +0000

    up to 4

---
+++ utils/fizzbuzz_kata/test_fizzbuzz_of_four.py
@@ -0,0 +1,3 @@
+from fizzbuzz import fizzbuzz
+def test_fizzbuzz_of_four():
+    assert fizzbuzz(4) == 4
--- utils/prompt.sh
+++ utils/prompt.sh
@@ -1,10 +1,11 @@
 ./execsh-gpt.py "
 continue the fizzbuzz kata - up to 4
 only do the above, nothing else
-use TDD
 the code is in fizzbuzz_kata/, cd just once, you're using a shell
+follow the same style as the existing code
+run the tests as often as possible - just run `pytest` - before and after each change
+use TDD
 always read code before changing it
-also - run the tests as often as possible - just run `pytest` - before and after each change
 work in tiny steps
 validate after each step
 change code by overwriting complete files, don't attempt to change code in place
