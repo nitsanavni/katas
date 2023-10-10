f18937be44df010cb032c36059e19e97b146942a
Nitsan Avni <nitsanav@gmail.com>
2023-10-10 16:25:30 +0000

    get_grids

--- sudoku-validator/tests.sh
+++ sudoku-validator/tests.sh
@@ -6,6 +6,7 @@ def get_rows:
   split("\n") |
   map(select(length>0) | split(" ") | map(tonumber));
     def get_columns: get_rows|transpose;
+    def get_grids: get_rows as $r | [range(3) as $i | range(3) as $j | ($r[3*$i:3*($i+1)]|map(.[3*$j:3*($j+1)])|add)];
 '
 
 echo test: check a row
@@ -29,6 +30,14 @@ echo test: get columns
 echo ""
 result=$(echo -e $input | jq -csR "$lib"'get_columns')
 echo -e "$input | get_columns -> $result"
+
+echo ""
+echo ""
+echo test: get grids
+echo ""
+input="1 1 1 2 2 2 3 3 3\n1 1 1 2 2 2 3 3 3\n1 1 1 2 2 2 3 3 3\n4 4 4 5 5 5 6 6 6\n4 4 4 5 5 5 6 6 6\n4 4 4 5 5 5 6 6 6\n7 7 7 8 8 8 9 9 9\n7 7 7 8 8 8 9 9 9\n7 7 7 8 8 8 9 9 9"
+result=$(echo -e $input | jq -csR "$lib"'get_grids')
+echo -e "$input | get_grids -> $result"
 
 
 
