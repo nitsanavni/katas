03cfc554eba1cfa189b9e093257009c5fd311d1d
Nitsan Avni <nitsanav@gmail.com>
2023-10-15 07:06:20 +0000

    e2e xfail test

---
+++ sudoku-validator-3/test_sudoku_validator.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def validate(puzzle):
+    return True
+
+
+@pytest.mark.xfail
+def test_validate_ones():
+    puzzle = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]
+    assert not validate(puzzle)
