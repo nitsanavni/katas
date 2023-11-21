770cbdd7e08298944b31787d2d7d7ad95d8c6ea3
Nitsan Avni <nitsanav@gmail.com>
2023-11-21 20:39:18 +0100

    3 -> Bingo

--- fizzbuzz-with-marvin.ai_fn/round_1.py
+++ fizzbuzz-with-marvin.ai_fn/round_1.py
@@ -3,11 +3,15 @@
 import marvin
 
 marvin.settings.llm_model = "openai/gpt-4-1106-preview"
+marvin.settings.llm_temperature = 0
 
 
 @marvin.ai_fn
 def numbers_1_to_100() -> list[str]:
-    """Returns a list of numbers from 1 to 100 as strings."""
+    """
+    Returns a list of numbers from 1 to 100 as strings.
+    exception to the rule: if the number is exactly 3 return "Bingo" instead.
+    """
 
 
 def main():
