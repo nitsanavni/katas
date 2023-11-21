a8b54ed45d63807923bd5df54b20e86e3732f967
Nitsan Avni <nitsanav@gmail.com>
2023-11-21 21:10:00 +0100

    instead of specifying formatting, use a pydantic.BaseModel

--- fizzbuzz-with-marvin.ai_fn/round_1.py
+++ fizzbuzz-with-marvin.ai_fn/round_1.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
 
 import marvin
+from pydantic import BaseModel
 
 marvin.settings.llm_model = "openai/gpt-4-1106-preview"
 marvin.settings.llm_temperature = 0
 
 
+class Transformation(BaseModel):
+    n: int
+    t: str
+
+    def __str__(self):
+        return f"{self.n} -> {self.t}"
+
+
 @marvin.ai_fn
-def numbers_1_to_25_with_exceptions() -> list[str]:
+def number_transformations_1_to_25() -> list[Transformation]:
     """
-    Returns a list of number transformations from 1 to 25 as strings.
+    Returns a list of number transformations from 1 to 25.
     each number is transformed
-    the rule is that the transformation returns the number itself (identity)
+    the rule is that the transformation returns the string representation of the number itself
     exceptions to the rule, for numbers 3, 5, 6, 9 and 10:
         if the number is either exactly 3, exactly 6 or exactly 9 transform it to "Bingo" instead.
         if the number is either exactly 5 or exactly 10 transform it to "Combo" instead.
-
-    follow this element format examples: "1 -> 1", "2 -> 2", "3 -> Bingo"
-
     """
 
 
 def main():
-    print("\n".join(numbers_1_to_25_with_exceptions()))
+    print("\n".join(map(str, number_transformations_1_to_25())))
 
 
 if __name__ == "__main__":
