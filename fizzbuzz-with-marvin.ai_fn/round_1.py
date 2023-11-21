5e1cadcef88e3dd6f8fd34aebbd02824cbc36607
Nitsan Avni <nitsanav@gmail.com>
2023-11-21 20:57:02 +0100

    returning transformations help

--- fizzbuzz-with-marvin.ai_fn/round_1.py
+++ fizzbuzz-with-marvin.ai_fn/round_1.py
@@ -9,10 +9,15 @@ marvin.settings.llm_temperature = 0
 @marvin.ai_fn
 def numbers_1_to_25_with_exceptions() -> list[str]:
     """
-    Returns a list of numbers from 1 to 25 as strings.
+    Returns a list of number transformations from 1 to 25 as strings.
+    each number is transformed
+    the rule is that the transformation returns the number itself (identity)
     exceptions to the rule, for numbers 3, 5, 6, 9 and 10:
-        if the number is either exactly 3, exactly 6 or exactly 9 return "Bingo" instead.
-        if the number is exactly 5 or 10 return "Combo" instead.
+        if the number is either exactly 3, exactly 6 or exactly 9 transform it to "Bingo" instead.
+        if the number is either exactly 5 or exactly 10 transform it to "Combo" instead.
+
+    follow this element format examples: "1 -> 1", "2 -> 2", "3 -> Bingo"
+
     """
 
 
