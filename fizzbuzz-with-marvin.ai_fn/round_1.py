39387a6810edca6e9559b707ca1d34450c0653be
Nitsan Avni <nitsanav@gmail.com>
2023-11-21 21:11:53 +0100

    multiples of 3

--- fizzbuzz-with-marvin.ai_fn/round_1.py
+++ fizzbuzz-with-marvin.ai_fn/round_1.py
@@ -21,8 +21,8 @@ def number_transformations_1_to_25() -> list[Transformation]:
     Returns a list of number transformations from 1 to 25.
     each number is transformed
     the rule is that the transformation returns the string representation of the number itself
-    exceptions to the rule, for numbers 3, 5, 6, 9 and 10:
-        if the number is either exactly 3, exactly 6 or exactly 9 transform it to "Bingo" instead.
+    exceptions to the rule, for numbers [3, 6, 9, ...] and 5, 10:
+        if the number is divisible by 3 transform it to "Bingo" instead.
         if the number is either exactly 5 or exactly 10 transform it to "Combo" instead.
     """
 
