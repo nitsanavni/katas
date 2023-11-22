1b21e3d5502b6805da6785bdcc0dfcb2d50144e4
Nitsan Avni <nitsanav@gmail.com>
2023-11-22 22:14:57 +0100

    one more rule

--- fizzbuzz-with-marvin.ai_fn/round_3.py
+++ fizzbuzz-with-marvin.ai_fn/round_3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import marvin
+import sys
 
 marvin.settings.llm_model = "openai/gpt-4-1106-preview"
 # marvin.settings.llm_model = "openai/gpt-3.5-turbo-1106"
@@ -18,8 +19,10 @@ if __name__ == "__main__":
     code = implement(
         "def print_numbers(start=1, end=25) -> None:",
         """one number per line
-except for the number 3, in which case print Gonzo instead""",
+except for the number 3, in which case print Gonzo instead
+for the number 4, 5 and 6, print Buch instead""",
     )
+    print(code, file=sys.stderr)
     exec(
         f"""
 {code}
