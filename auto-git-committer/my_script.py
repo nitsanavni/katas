cf27737299148aad8fe87c1bb085e2820954431d
Nitsan Avni <nitsanav@gmail.com>
2023-11-23 16:14:58 +0000

    hypot

---
+++ auto-git-committer/my_script.py
@@ -0,0 +1,13 @@
+def greet_world():
+    print("Hello World!")
+
+
+# def calc_rect_area(w, h):
+# hypotenuse - the sqrt(x^2 + y^2), x,y - the height and width
+def hypot(x, y):
+    return 42
+
+
+def applesauce():
+    dummy = hypot(3, 4)
+    dummy2 = hypot(5, 12)
