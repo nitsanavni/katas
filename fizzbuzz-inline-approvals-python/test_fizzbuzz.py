18cbf463ff7aef6ace7175e2c170a3f711b3ecbb
Nitsan Avni <nitsanav@gmail.com>
2023-12-10 10:09:38 +0100

    extract table

--- fizzbuzz-inline-approvals-python/test_fizzbuzz.py
+++ fizzbuzz-inline-approvals-python/test_fizzbuzz.py
@@ -1,5 +1,71 @@
+#!/usr/bin/env python -m pytest -q
+
 from approvaltests import verify, Options
 from approvaltests.reporters import ReporterThatAutomaticallyApproves
+from functools import partial
+from itertools import zip_longest
+
+
+def verify_inline(*args, **kwargs):
+    return verify(
+        *args,
+        **kwargs,
+        options=Options().with_reporter(ReporterThatAutomaticallyApproves()).inline()
+    )
+
+
+def chunk(l: list, n: int) -> list[list]:
+    return [l[i : i + n] for i in range(0, len(l), n)]
+
+
+def test_chunk_9_3():
+    """
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    """
+    verify_inline(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+
+
+def test_chunk_9_4():
+    """
+    [[1, 2, 3, 4], [5, 6, 7, 8], [9]]
+    """
+    verify_inline(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9], 4))
+
+
+def table(rows: list[list[str]]) -> str:
+    _widths = widths(rows)
+    rows = [
+        [
+            (row[i] if i < len(row) else "").ljust(_widths[i])
+            for i, w in enumerate(_widths)
+        ]
+        for row in rows
+    ]
+    rows = ["| " + " | ".join(row) + " |" for row in rows]
+    return "\n".join(rows)
+
+
+def widths(rows: list[list[str]]) -> list[int]:
+    return [max(len(cell or "") for cell in col) for col in zip_longest(*rows)]
+
+
+def test_widths():
+    """
+    [20, 8, 9]
+    """
+    verify_inline(widths(chunk([str(i) * i for i in range(1, 11)], 3)))
+
+
+def test_table():
+    """
+    | 1  | 2  | 3  | 4  |
+    | 5  | 6  | 7  | 8  |
+    | 9  | 10 | 11 | 12 |
+    | 13 | 14 | 15 | 16 |
+    | 17 | 18 | 19 | 20 |
+    | 21 | 22 | 23 |    |
+    """
+    verify_inline(table(chunk([str(i) for i in range(1, 24)], 4)))
 
 
 def fizzbuzz(up_to=25):
@@ -14,41 +80,20 @@ def fizzbuzz(up_to=25):
             else str(i)
         )
 
-    # draw a table, each row has up to 5 cells
-    # each cell is the result of fb(i) for i in range(row * 5, row * 5 + 5)
-    s = 10
-    rows = [
-        [fb(i) for i in range(row * s + 1, row * s + s + 1)]
-        for row in range(up_to // s)
-    ]
-
-    # find max width for each column
-    widths = [max(len(cell) for cell in col) for col in zip(*rows)]
-
-    # format each cell with the right width
-    rows = [[cell.ljust(widths[i]) for i, cell in enumerate(row)] for row in rows]
-
-    # join each row with a pipe, including the edges
-    rows = ["|" + "|".join(row) + "|" for row in rows]
-
-    # join all rows with a newline
-    return "\n".join(rows)
+    return table(chunk([fb(i) for i in range(1, up_to + 1)], 11))
 
 
 def test_fizzbuzz():
     """
-    |1   |2   |Fizz|4   |Buzz    |Fizz|7   |8   |Fizz|Buzz    |
-    |11  |Fizz|13  |14  |FizzBuzz|16  |17  |Fizz|19  |Buzz    |
-    |Fizz|22  |23  |Fizz|Buzz    |26  |Fizz|28  |29  |FizzBuzz|
-    |31  |32  |Fizz|34  |Buzz    |Fizz|37  |38  |Fizz|Buzz    |
-    |41  |Fizz|43  |44  |FizzBuzz|46  |47  |Fizz|49  |Buzz    |
-    |Fizz|52  |53  |Fizz|Buzz    |56  |Fizz|58  |59  |FizzBuzz|
-    |61  |62  |Fizz|64  |Buzz    |Fizz|67  |68  |Fizz|Buzz    |
-    |71  |Fizz|73  |74  |FizzBuzz|76  |77  |Fizz|79  |Buzz    |
-    |Fizz|82  |83  |Fizz|Buzz    |86  |Fizz|88  |89  |FizzBuzz|
-    |91  |92  |Fizz|94  |Buzz    |Fizz|97  |98  |Fizz|Buzz    |
+    | 1        | 2        | Fizz | 4        | Buzz     | Fizz | 7    | 8        | Fizz     | Buzz | 11   |
+    | Fizz     | 13       | 14   | FizzBuzz | 16       | 17   | Fizz | 19       | Buzz     | Fizz | 22   |
+    | 23       | Fizz     | Buzz | 26       | Fizz     | 28   | 29   | FizzBuzz | 31       | 32   | Fizz |
+    | 34       | Buzz     | Fizz | 37       | 38       | Fizz | Buzz | 41       | Fizz     | 43   | 44   |
+    | FizzBuzz | 46       | 47   | Fizz     | 49       | Buzz | Fizz | 52       | 53       | Fizz | Buzz |
+    | 56       | Fizz     | 58   | 59       | FizzBuzz | 61   | 62   | Fizz     | 64       | Buzz | Fizz |
+    | 67       | 68       | Fizz | Buzz     | 71       | Fizz | 73   | 74       | FizzBuzz | 76   | 77   |
+    | Fizz     | 79       | Buzz | Fizz     | 82       | 83   | Fizz | Buzz     | 86       | Fizz | 88   |
+    | 89       | FizzBuzz | 91   | 92       | Fizz     | 94   | Buzz | Fizz     | 97       | 98   | Fizz |
+    | Buzz     | 101      |      |          |          |      |      |          |          |      |      |
     """
-    verify(
-        fizzbuzz(101),
-        options=Options().with_reporter(ReporterThatAutomaticallyApproves()).inline(),
-    )
+    verify_inline(fizzbuzz(101))
