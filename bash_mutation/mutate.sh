14fe41abbe2df0e5f5603c937b8e0f131880b0ed
Nitsan Avni <nitsanav@gmail.com>
2024-01-01 17:56:56 +0100

    cleaner

--- bash_mutation/mutate.sh
+++ bash_mutation/mutate.sh
@@ -3,6 +3,8 @@
 mutations=(
     "10s/!=/==/"
     "10s/!=/==/2"
+    "10s/\[\[.*\]\]/true/"
+    "10s/\[\[.*\]\]/false/"
 )
 
 for mutation in "${mutations[@]}"; do
@@ -11,11 +13,10 @@ for mutation in "${mutations[@]}"; do
     mutated_script=$(mktemp)
     sed "$mutation" gilded_rose.sh >"$mutated_script"
 
-    echo "diff -u gilded_rose.sh $mutated_script"
-    diff -u gilded_rose.sh "$mutated_script"
+    diff gilded_rose.sh "$mutated_script"
 
     echo ""
-    
+
     ./test.sh "$mutated_script"
 
     if [ $? -eq 0 ]; then
--- bash_mutation/test.sh
+++ bash_mutation/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mutated_script="$1"
-input=$(jq -rn '[["Some Item"],[0,1],[0]]|combinations|join("|")')
+input=$(jq -rn '[["Some Item", "Aged Brie"],[0,1],[0]]|combinations|join("|")')
 
 if [ "$UPDATE" == "1" ]; then
     echo "$input" | bash "$mutated_script" >approved
 else
-    echo "$input" | bash "$mutated_script" | diff -u - approved
+    echo "$input" | bash "$mutated_script" | diff -q - approved
 fi
