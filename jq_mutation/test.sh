94cd816349eabe1bab2690a45a6cb0d8b1572e0d
Nitsan Avni <nitsanav@gmail.com>
2024-01-02 16:33:31 +0100

    kill all mutants

--- jq_mutation/test.sh
+++ jq_mutation/test.sh
@@ -2,9 +2,9 @@
 
 mutated_script=${1:-gilded_rose}
 input=$(jq -rn '[[
-    ["Some Item"],
-    [0],
-    [0]
+    ["Some Item", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"],
+    [0,1,11,6,-1],
+    [0,1,-1,50,49]
 ]|combinations|({name:.[0],sell_in:.[1],quality:.[2]})]')
 
 if [ "$UPDATE" == "1" ]; then
