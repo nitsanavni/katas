b0307af2edd3963b03157932faa8294af0a51ab7
Nitsan Avni <nitsanav@gmail.com>
2024-01-03 17:34:51 +0100

    . d comments

--- jq_mutation/mutate.sh
+++ jq_mutation/mutate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# find specific mutations in the script
 mutations=$(python mutations.py)
 
 # split on newlines
@@ -8,12 +9,15 @@ IFS=$'\n' read -rd '' -a mutations <<<"$mutations"
 for mutation in "${mutations[@]}"; do
     echo "Testing mutation: $mutation"
 
+    # temp file for mutated script
     module=$(basename $(mktemp -t gilded_rose -p .))
     script="$module.jq"
 
     echo $mutation | grep "inc int" >/dev/null
 
     if [ $? -eq 0 ]; then
+        # most mutations are just sed replacements
+        # but this one is handled by a python script
         python inc_int.py gilded_rose.jq $(echo $mutation | grep -oE '\d+') >"$script"
     else
         sed "$mutation" gilded_rose.jq >"$script"
@@ -21,16 +25,20 @@ for mutation in "${mutations[@]}"; do
 
     rm -f "$module"
 
+    # show what changed (mutated)
     diff gilded_rose.jq "$script"
 
     echo ""
 
+    # execute the test against the mutated script
     ./test.sh "$module"
 
     if [ $? -eq 0 ]; then
+        # tests passed, mutation survived - a potential gap in our tests
         echo ""
         echo "  Mutation $mutation survived !!"
     else
+        # tests failed, mutation killed (caught by tests)
         echo ""
         echo "  Mutation $mutation killed !!"
     fi
