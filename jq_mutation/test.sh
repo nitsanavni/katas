01f933e98bdc768634775d1a3a511bd854d4a864
Nitsan Avni <nitsanav@gmail.com>
2024-01-04 09:50:22 +0100

    . t kill one more mutation

--- jq_mutation/test.sh
+++ jq_mutation/test.sh
@@ -4,7 +4,7 @@ mutated_script=${1:-gilded_rose}
 input=$(jq -rn '[[
     ["Some Item", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"],
     [0,1,11,6,-1],
-    [0,1,-1,50,49]
+    [0,1,-1,50,49,2]
 ]|combinations|({name:.[0],sell_in:.[1],quality:.[2]})]')
 
 if [ "$UPDATE" == "1" ]; then
@@ -12,5 +12,3 @@ if [ "$UPDATE" == "1" ]; then
 else
     echo "$input" | jq -r "include \"$mutated_script\"; update_quality" | diff - approved
 fi
-
-
