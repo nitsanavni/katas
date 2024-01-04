78e2219e94bd4a53f91e9a8bed098ae82a6eaeef
Nitsan Avni <nitsanav@gmail.com>
2024-01-04 09:58:28 +0100

    . t apprpvals: one item per line

--- jq_mutation/test.sh
+++ jq_mutation/test.sh
@@ -7,8 +7,10 @@ input=$(jq -rn '[[
     [0,1,-1,50,49,2]
 ]|combinations|({name:.[0],sell_in:.[1],quality:.[2]})]')
 
+received=$(echo "$input" | jq -r "include \"$mutated_script\"; update_quality | .[] | ([.name,.sell_in,.quality|tostring])|join(\",\")")
+
 if [ "$UPDATE" == "1" ]; then
-    echo "$input" | jq -r "include \"$mutated_script\"; update_quality" >approved
+    echo "$received" >approved
 else
-    echo "$input" | jq -r "include \"$mutated_script\"; update_quality" | diff - approved
+    echo "$received" | diff - approved
 fi
