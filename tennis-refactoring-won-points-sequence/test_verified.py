01d5b1362b9c8adff29b46e9ca9eb1605ded247b
Nitsan Avni <nitsanav@gmail.com>
2024-02-13 18:11:00 +0100

    copy split_code

--- tennis-refactoring-won-points-sequence/test_verified.py
+++ tennis-refactoring-won-points-sequence/test_verified.py
@@ -1,8 +1,13 @@
 from approvaltests import verify, Options
 
+# import from file split_code.py
+from split_code import SplitCode
+
 
 def verified(func):
     func.get_docstring = lambda: func.__doc__
+    func.get_path = lambda: func.__code__.co_filename
+    func.get_own_name = lambda: func.__name__
 
     return func
 
@@ -13,6 +18,26 @@ def greet(name):
     Bob -> Hello, Bob!
     """
     return f"Hello, {name}!"
+
+
+def test_verified_sees_func_name():
+    """
+    greet
+    """
+    verify(
+        f"{greet.get_own_name()}",
+        options=Options().inline(),
+    )
+
+
+def test_verified_knows_func_file():
+    """
+    the file of greet function: test_verified.py
+    """
+    verify(
+        f'the file of greet function: {greet.get_path().split("/")[-1]}',
+        options=Options().inline(),
+    )
 
 
 def test_verified_sees_func_docstring():
