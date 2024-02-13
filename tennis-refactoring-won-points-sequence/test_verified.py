34d5761084960cdfecdf17aed2aa6b51a4bc9d46
Nitsan Avni <nitsanav@gmail.com>
2024-02-13 18:22:56 +0100

    reformat_docstring

--- tennis-refactoring-won-points-sequence/test_verified.py
+++ tennis-refactoring-won-points-sequence/test_verified.py
@@ -5,9 +5,29 @@ from split_code import SplitCode
 
 
 def verified(func):
+    def input(line):
+        return line.split(" -> ")[0].strip()
+
+    def format(line):
+        i = input(line)
+        if i:
+            return f"{i} -> {func(i)}"
+        else:
+            return line
+
     func.get_docstring = lambda: func.__doc__
     func.get_path = lambda: func.__code__.co_filename
     func.get_own_name = lambda: func.__name__
+    func.get_full_file_code = lambda: open(func.get_path()).read()
+    func.inputs = lambda: [
+        input(l) for l in func.get_docstring().splitlines() if l.strip()
+    ]
+    func.reformat_docstring = lambda: "\n".join(
+        [format(l) for l in func.get_docstring().splitlines()]
+    )
+
+    def verify():
+        split = SplitCode.on_method(func.get_full_file_code(), func.get_own_name())
 
     return func
 
@@ -18,6 +38,26 @@ def greet(name):
     Bob -> Hello, Bob!
     """
     return f"Hello, {name}!"
+
+
+def test_verified_reformats_docstring():
+    """
+    ['', 'Bob -> Hello, Bob!', '    ']
+    """
+    verify(
+        greet.reformat_docstring().splitlines(),
+        options=Options().inline(),
+    )
+
+
+def test_verified_inputs():
+    """
+    Bob
+    """
+    verify(
+        "\n".join(greet.inputs()),
+        options=Options().inline(),
+    )
 
 
 def test_verified_sees_func_name():
