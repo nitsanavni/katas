dcc8386f2412c3387725a123d1d9ae9d91e2a3f6
Nitsan Avni <nitsanav@gmail.com>
2024-05-14 22:40:22 +0200

    wip plan

--- do_one_thing/test_do_one_thing.py
+++ do_one_thing/test_do_one_thing.py
@@ -2,6 +2,9 @@ from typing import List
 from filecache import filecache
 import openai
 import subprocess
+import tempfile
+import shutil
+import os
 from approvaltests import verify, Options
 from approvaltests.inline.inline_options import InlineOptions
 
@@ -42,3 +45,51 @@ def test_exec():
     """
     verify(exec("printf 'Hello, world!'")[0], options=semi)
     verify(exec("printf 'Hello, world!' 1>&2")[1], options=semi)
+
+
+class Plan:
+    def __init__(self, file):
+        self.file = file
+        if os.stat(self.file.name).st_size == 0:
+            self.file.write("** empty plan **")
+
+    def __str__(self) -> str:
+        self.file.seek(0)
+        return self.file.read()
+
+    def add(self, task):
+        self.file.seek(0)
+        content = self.file.read()
+        self.file.seek(0)
+        if content == "** empty plan **":
+            self.file.truncate(0)
+            self.file.write(f"- [ ] **{task}**")
+        else:
+            self.file.write(f"{content}\n- [ ] **{task}**")
+
+
+def test_empty_plan():
+    """
+    ** empty plan **
+    """
+    path = "tmp_plan.md"
+    if os.path.exists(path):
+        os.remove(path)
+    plan = Plan(file=open(path, mode="w+"))
+    verify(plan, options=semi)
+    if os.path.exists(path):
+        os.remove(path)
+
+
+def test_add_to_plan():
+    """
+    - [ ] **get the date**
+    """
+    path = "tmp_plan_2.md"
+    if os.path.exists(path):
+        os.remove(path)
+    plan = Plan(file=open(path, mode="w+"))
+    plan.add("get the date")
+    verify(plan, options=semi)
+    if os.path.exists(path):
+        os.remove(path)
