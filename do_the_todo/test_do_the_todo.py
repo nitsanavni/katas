ecf212f790e7eff731feb8827b41c65f45b1fb95
Nitsan Avni <nitsanav@gmail.com>
2024-05-16 10:30:23 +0200

    do the todo: naive prompt

--- do_the_todo/test_do_the_todo.py
+++ do_the_todo/test_do_the_todo.py
@@ -37,3 +37,26 @@ def test_prompt():
         prompt("knock knock"),
         options=Options().inline(InlineOptions.semi_automatic()),
     )
+
+
+def do_the_todo(code: str):
+    return prompt(f"Do the TODO in the following code:\n{code}")
+
+def test_do_the_todo():
+    """
+    Sure, here is the code with a more descriptive function name:
+    
+    ```python
+    # Name the function to better reflect its purpose
+    def greet_user(name):
+        return f"Hello, {name}!"
+    ``` 
+    
+    The new name `greet_user` makes it clear that the function is intended to greet a user by their name.
+    ***** DELETE ME TO APPROVE *****
+    """
+    code = """# TODO: give a better name to this function
+def applesauce(x):
+    return f"Hello, {x}!"
+"""
+    verify(do_the_todo(code), options=Options().inline(InlineOptions.semi_automatic()))
