381d89bb064dde659da19301dee8fcfafd279a3c
Nitsan Avni <nitsanav@gmail.com>
2024-08-31 23:03:08 +0200

    - r mv onKey to file

---
+++ mm/onKey.ts
@@ -0,0 +1,42 @@
+import { render } from "./render"; // Import the render function
+
+export const onKey = (state) => (str, key) => {
+  if (state.inEditMode()) {
+    if (key.name === "left") {
+      state.cursorLeft();
+    } else if (key.name === "right") {
+      state.cursorRight();
+    } else if (key.name === "backspace") {
+      state.deleteChar();
+    } else if (key.name === "escape") {
+      state.toggleEditMode();
+    } else if (key.name === "return") {
+      state.addItem();
+    } else if (str && !key.ctrl && !key.meta) {
+      state.addChar(str);
+    }
+  } else {
+    if (key.name === "q") {
+      exitProgram();
+    } else if (key.name === "up") {
+      state.moveUp();
+    } else if (key.name === "down") {
+      state.moveDown();
+    } else if (key.name === "e" || key.name === "escape") {
+      state.toggleEditMode();
+    }
+  }
+
+  render(
+    state.listItems(),
+    state.selectedIndex(),
+    state.cursorPos(),
+    state.inEditMode(),
+  );
+};
+
+function exitProgram() {
+  process.stdout.write("\x1B[?25h"); // Show the cursor
+  console.log("Exiting...");
+  process.exit();
+}
