ca3c5104889faf284e1a7e7e1c4f50d90c59daa4
Nitsan Avni <nitsanav@gmail.com>
2024-09-02 21:16:59 +0200

    . d

--- mm/cat.ts
+++ mm/cat.ts
@@ -1,5 +1,35 @@
 import stringLength from "string-length";
 
+/**
+ * Concatenates two multi-line strings, treating them as rectangular blocks.
+ * It aligns the lines of both strings based on the maximum length of the lines
+ * in the first string (lhs) and pads the lines of both strings to ensure they
+ * have the same number of lines.
+ *
+ * Example:
+ *
+ * Input:
+ * lhs:
+ * Mindmaps clarify
+ * thoughts and
+ * ideas.
+ *
+ * rhs:
+ * Organize
+ * concepts
+ * visually
+ * with mindmaps.
+ *
+ * Result:
+ * Mindmaps clarifyOrganize
+ * thoughts and    concepts
+ * ideas.          visually
+ *                 with mindmaps.
+ *
+ * @param lhs - The first string to concatenate.
+ * @param rhs - The second string to concatenate.
+ * @returns - The concatenated multi-line string.
+ */
 export const cat = (lhs: string, rhs: string): string => {
   const lhsLines = lhs.split("\n");
   const rhsLines = rhs.split("\n");
