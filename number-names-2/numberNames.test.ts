11f3db63436f24ef56eeb534fa446e0e93f3dd68
Nitsan Avni <nitsanav@gmail.com>
2024-09-07 00:21:34 +0200

    - F define missing function with empty body

--- number-names-2/numberNames.test.ts
+++ number-names-2/numberNames.test.ts
@@ -1,5 +1,9 @@
 import { expect, test } from "bun:test";
 
+function numberName(num: number): string {
+  // empty body
+}
+
 test("1 should be 'one'", () => {
   expect(numberName(1)).toBe('one');
 });
