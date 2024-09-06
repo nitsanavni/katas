0ce23f8c2308a29ffc8e92a7c6514300336ac9e7
Nitsan Avni <nitsanav@gmail.com>
2024-09-07 01:08:12 +0200

    . t cleanup test comments

--- number-names-2/numberNames.test.ts
+++ number-names-2/numberNames.test.ts
@@ -23,25 +23,25 @@ const testCases: [number, string][] = [
   [19, 'nineteen'],
   [20, 'twenty'],
   [21, 'twenty-one'],
-  [25, 'twenty-five'], // Added additional random test case
-  [30, 'thirty'], // Added additional random test case
-  [35, 'thirty-five'], // Added additional random test case
-  [42, 'forty-two'], // Added additional random test case
-  [50, 'fifty'], // Added additional random test case
-  [67, 'sixty-seven'], // Added additional random test case
-  [75, 'seventy-five'], // Added additional random test case
-  [89, 'eighty-nine'], // Added additional random test case
-  [97, 'ninety-seven'], // Added additional random test case
-  [100, 'one hundred'], // Added test case for number 100
-  [101, 'one hundred one'], // New additional test case
-  [123, 'one hundred twenty-three'], // New additional test case
-  [250, 'two hundred fifty'], // New additional test case
-  [305, 'three hundred five'], // New additional test case
-  [412, 'four hundred twelve'], // New additional test case
-  [598, 'five hundred ninety-eight'], // New additional test case
-  [700, 'seven hundred'], // New additional test case
-  [850, 'eight hundred fifty'], // New additional test case
-  [999, 'nine hundred ninety-nine'] // New additional test case
+  [25, 'twenty-five'],
+  [30, 'thirty'],
+  [35, 'thirty-five'],
+  [42, 'forty-two'],
+  [50, 'fifty'],
+  [67, 'sixty-seven'],
+  [75, 'seventy-five'],
+  [89, 'eighty-nine'],
+  [97, 'ninety-seven'],
+  [100, 'one hundred'],
+  [101, 'one hundred one'],
+  [123, 'one hundred twenty-three'],
+  [250, 'two hundred fifty'],
+  [305, 'three hundred five'],
+  [412, 'four hundred twelve'],
+  [598, 'five hundred ninety-eight'],
+  [700, 'seven hundred'],
+  [850, 'eight hundred fifty'],
+  [999, 'nine hundred ninety-nine']
 ];
 
 testCases.forEach(([input, expected]) => {
