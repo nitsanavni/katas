0a614846c9da20fa1c0de127312f02380d016398
Nitsan Avni <nitsanav@gmail.com>
2024-09-07 00:34:30 +0200

    . t r tuples

--- number-names-2/numberNames.test.ts
+++ number-names-2/numberNames.test.ts
@@ -1,20 +1,20 @@
 import { expect, test } from "bun:test";
 import { numberName } from "./numberNames";
 
-const testCases = [
-  { input: 1, expected: 'one' },
-  { input: 2, expected: 'two' },
-  { input: 3, expected: 'three' },
-  { input: 4, expected: 'four' },
-  { input: 5, expected: 'five' },
-  { input: 6, expected: 'six' },
-  { input: 7, expected: 'seven' },
-  { input: 8, expected: 'eight' },
-  { input: 9, expected: 'nine' },
-  { input: 10, expected: 'ten' },
+const testCases: [number, string][] = [
+  [1, 'one'],
+  [2, 'two'],
+  [3, 'three'],
+  [4, 'four'],
+  [5, 'five'],
+  [6, 'six'],
+  [7, 'seven'],
+  [8, 'eight'],
+  [9, 'nine'],
+  [10, 'ten'],
 ];
 
-testCases.forEach(({ input, expected }) => {
+testCases.forEach(([input, expected]) => {
   test(`${input} should be '${expected}'`, () => {
     expect(numberName(input)).toBe(expected);
   });
