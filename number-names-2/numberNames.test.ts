847b0b3fc58100264dd8a23b4cbf856a405837aa
Nitsan Avni <nitsanav@gmail.com>
2024-09-07 00:28:55 +0200

    - r move numberName function to new file

--- number-names-2/numberNames.test.ts
+++ number-names-2/numberNames.test.ts
@@ -1,8 +1,5 @@
 import { expect, test } from "bun:test";
-
-function numberName(num: number): string {
-  // empty body
-}
+import { numberName } from "./numberNames";
 
 test("1 should be 'one'", () => {
   expect(numberName(1)).toBe('one');
