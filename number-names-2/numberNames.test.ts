69edf867e809951ccb476665dfae069a4fd0db80
Nitsan Avni <nitsanav@gmail.com>
2024-09-07 00:52:20 +0200

    . t sample 21-100

--- number-names-2/numberNames.test.ts
+++ number-names-2/numberNames.test.ts
@@ -22,7 +22,17 @@ const testCases: [number, string][] = [
   [18, 'eighteen'],
   [19, 'nineteen'],
   [20, 'twenty'],
-  [21, 'twenty-one'], // Added test case for number 21
+  [21, 'twenty-one'],
+  [25, 'twenty-five'], // Added additional random test case
+  [30, 'thirty'], // Added additional random test case
+  [35, 'thirty-five'], // Added additional random test case
+  [42, 'forty-two'], // Added additional random test case
+  [50, 'fifty'], // Added additional random test case
+  [67, 'sixty-seven'], // Added additional random test case
+  [75, 'seventy-five'], // Added additional random test case
+  [89, 'eighty-nine'], // Added additional random test case
+  [97, 'ninety-seven'], // Added additional random test case
+  [100, 'one hundred'] // Added test case for number 100
 ];
 
 testCases.forEach(([input, expected]) => {
