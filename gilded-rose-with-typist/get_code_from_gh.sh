542dba7edf6b6cb73c4bf1371c7aa8bf38c79676
Nitsan Avni <nitsanav@gmail.com>
2024-09-28 19:09:37 +0200

    - F just the repo name

--- gilded-rose-with-typist/get_code_from_gh.sh
+++ gilded-rose-with-typist/get_code_from_gh.sh
@@ -1,6 +1,6 @@
 # goal: get the python gilded rose code into a local file
 
 # 1. find emily bache's gilded rose repo on github
-gilded_rose_repo=$(gh search repos --owner emilybache rose --limit 1)
+gilded_rose_repo=$(gh search repos --owner emilybache rose --limit 1 --json name)
 
 echo "repo: $gilded_rose_repo"
