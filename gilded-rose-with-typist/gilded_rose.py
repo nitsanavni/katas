e17b990c17881c4ea5cd1e234af2c827530a89ba
Nitsan Avni <nitsanav@gmail.com>
2024-10-01 22:25:23 +0200

    - r two checkboxes

--- gilded-rose-with-typist/gilded_rose.py
+++ gilded-rose-with-typist/gilded_rose.py
@@ -16,54 +16,28 @@ class GildedRose(object):
                     self.update_item(item)
 
     def update_item(self, item):
-        if item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality -= 1
-        else:
-            if item.quality < 50:
-                item.quality += 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
+        if item.quality > 0:
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality -= 1
         if item.name != "Sulfuras, Hand of Ragnaros":
             item.sell_in -= 1
         if item.sell_in < 0:
-            if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality -= 1
-            else:
-                item.quality = 0
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    item.quality -= 1
 
     def update_backstage(self, item):
-        if item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality -= 1
-        else:
-            if item.quality < 50:
-                item.quality += 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in -= 1
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11:
+                if item.quality < 50:
+                    item.quality += 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality += 1
+        item.sell_in -= 1
         if item.sell_in < 0:
-            if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality -= 1
-            else:
-                item.quality = 0
+            item.quality = 0
 
     def update_brie(self, item):
         if item.quality < 50:
--- gilded-rose-with-typist/prompt.md
+++ gilded-rose-with-typist/prompt.md
@@ -7,7 +7,7 @@ x = backstage
 just do the next unchecked step, do it exactly, don't jump ahead, for every step all other code stays the exact same:
 
 - [x] in the loop, surround update_item call with if name is $x, both branches call update_item
-- [ ] new function update_$x, *exact same* as update_item, used in the right branch
+- [x] new function update_$x, *exact same* as update_item, used in the right branch
 - [ ] in update_$x:
       get rid of all conditions on name, keeping only true branches
       everything else stay the same
