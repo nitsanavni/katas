71a8a67cc7b3344a469a43cb05a7a3e3f0becdc9
Nitsan Avni <nitsanav@gmail.com>
2024-10-01 22:22:27 +0200

    - r 2nd checkbox

--- gilded-rose-with-typist/gilded_rose.py
+++ gilded-rose-with-typist/gilded_rose.py
@@ -11,11 +11,36 @@ class GildedRose(object):
                 self.update_brie(item)
             else:
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    self.update_item(item)
+                    self.update_backstage(item)
                 else:
                     self.update_item(item)
 
     def update_item(self, item):
+        if item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    item.quality -= 1
+        else:
+            if item.quality < 50:
+                item.quality += 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality += 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality += 1
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in -= 1
+        if item.sell_in < 0:
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality -= 1
+            else:
+                item.quality = 0
+
+    def update_backstage(self, item):
         if item.name != "Backstage passes to a TAFKAL80ETC concert":
             if item.quality > 0:
                 if item.name != "Sulfuras, Hand of Ragnaros":
--- gilded-rose-with-typist/prompt.md
+++ gilded-rose-with-typist/prompt.md
@@ -6,7 +6,7 @@ x = backstage
 
 just do the next unchecked step, do it exactly, don't jump ahead, for every step all other code stays the exact same:
 
-- [ ] in the loop, surround update_item call with if name is $x, both branches call update_item
+- [x] in the loop, surround update_item call with if name is $x, both branches call update_item
 - [ ] new function update_$x, *exact same* as update_item, used in the right branch
 - [ ] in update_$x:
       get rid of all conditions on name, keeping only true branches
