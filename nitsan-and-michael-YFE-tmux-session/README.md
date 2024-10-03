8d97539bf5ee1abf0f85767aaefe739a488d3c04
Nitsan Avni <nitsanav@gmail.com>
2024-10-03 15:59:47 +0000

    initial commit: failig test

---
+++ nitsan-and-michael-YFE-tmux-session/README.md
@@ -0,0 +1,8 @@
+# experiment pairing in tmux with both emacs and nvim
+
+I remember some vi(2) commands.
+
+
+Here we go...
+
+
---
+++ nitsan-and-michael-YFE-tmux-session/roman_test.py
@@ -0,0 +1,6 @@
+def arabic_to_roman(arabic_number):
+    return "0"
+
+
+def test_1000():
+    assert arabic_to_roman(1000) == "M"
