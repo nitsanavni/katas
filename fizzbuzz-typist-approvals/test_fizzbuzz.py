5f5b514325961ad8077412c58543357be2345226
Nitsan Avni <nitsanav@gmail.com>
2024-10-03 10:01:56 +0200

    Remove "seven" print statement.

--- fizzbuzz-typist-approvals/test_fizzbuzz.py
+++ fizzbuzz-typist-approvals/test_fizzbuzz.py
@@ -5,8 +5,6 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
-        elif i % 7 == 0:
-            print("seven")
         elif i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
