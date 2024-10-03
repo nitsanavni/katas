e76a62df67d6d17d769a7217340f90eeca91ea2d
Nitsan Avni <nitsanav@gmail.com>
2024-10-03 16:11:19 +0000

    MM

--- nitsan-and-michael-YFE-tmux-session/roman_test.py
+++ nitsan-and-michael-YFE-tmux-session/roman_test.py
@@ -1,6 +1,12 @@
 def arabic_to_roman(arabic_number):
+    if arabic_number > 1000:
+        return "MM"
     return "M"
 
 
 def test_1000():
     assert arabic_to_roman(1000) == "M"
+
+
+def test_2000():
+    assert arabic_to_roman(2000) == "MM"
