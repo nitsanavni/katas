7e9eb23f81a7c4ff298ad8a9ba941f24ca6d8f0b
Nitsan Avni <nitsanav@gmail.com>
2024-10-03 09:59:34 +0200

    Add "seven" output for number 7.

--- fizzbuzz-typist-approvals/test_fizzbuzz.py
+++ fizzbuzz-typist-approvals/test_fizzbuzz.py
@@ -5,6 +5,8 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
+        elif i == 7:
+            print("seven")
         elif i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
