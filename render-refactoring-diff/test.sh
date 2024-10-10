db9b2050c844ef6dca7de351166b0adbba691f0b
Nitsan Avni <nitsanav@gmail.com>
2024-10-10 15:51:54 +0200

    . t r

--- render-refactoring-diff/test.sh
+++ render-refactoring-diff/test.sh
@@ -3,7 +3,12 @@ hello"
 changed="first line
 world!"
 
-git diff --word-diff --no-index <(echo -e "$original") <(echo -e "$changed") | \
-	grep @@ -A 100 | grep -v @@ | \
-	python git_diff_to_html.py > diff.html && \
-	open diff.html
+diff_with_header() {
+	git diff --word-diff --no-index <(echo -e "$1") <(echo -e "$2")
+}
+
+scrub_header() {
+	grep @@ -A 100 | grep -v @@
+}
+
+diff_with_header "$original" "$changed" | scrub_header | python git_diff_to_html.py > diff.html && open diff.html
